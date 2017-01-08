60196fd6d81b9968d6901d6c8a08a52ca0448166
Peter Milne <peter.milne@d-tacq.com>
2017-01-08 20:46:34 +0000

    has module count, handles modules 	modified:   acq400.py

--- acq400.py
+++ acq400.py
@@ -6,14 +6,26 @@ Created on Sun Jan  8 12:36:38 2017
 @author: pgm
 """
 
+import sys
 import netclient
 
 
 class Acq400:
     svc = {}
+    __mod_count = 0
+    @property 
+    def mod_count(self):
+        return self.__mod_count
+        
     def __init__(self, _uut):
         self.uut = _uut
-        self.svc["s0"] = netclient.Siteclient(self.uut, 4220)
+        s0 = self.svc["s0"] = netclient.Siteclient(self.uut, 4220)
+        sl = s0.SITELIST.split(",")
+        sl.pop(0)
+        for sm in sl:
+            site = int(sm.split("=").pop(0))
+            self.svc["s%d" % site] = netclient.Siteclient(self.uut, 4220+site)
+            self.__mod_count += 1
         
 
     def __getattr__(self, name):
@@ -26,10 +38,20 @@ class Acq400:
 
 if __name__ == '__main__':
    SERVER_ADDRESS = '10.12.132.18'
+   if len(sys.argv) > 1:
+        SERVER_ADDRESS = sys.argv[1]
 
+   print("create Acq400 %s" %(SERVER_ADDRESS))
    uut = Acq400(SERVER_ADDRESS)
    print("MODEL %s" %(uut.s0.MODEL))
    print("SITELIST %s" %(uut.s0.SITELIST))
+   print("MODEL %s" %(uut.s1.MODEL))
+   
+   print("Module count %d" % (uut.mod_count))
+   
+   for sx in sorted(uut.svc):
+       print("SITE:%s MODEL:%s" % (sx, uut.svc[sx].sr("MODEL")) )
+       
 
 
     
