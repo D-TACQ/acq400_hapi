f5f9af89ec2cdc3b9b2b2be3a4d091d81fcfa616
Peter Milne <peter.milne@d-tacq.com>
2018-01-12 19:29:44 +0000

    export AcqPorts 	modified:   __init__.py

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 from netclient import Netclient
 from netclient import Siteclient
 from netclient import Logclient
-from acq400 import Acq400, STATE
+from acq400 import Acq400, STATE, AcqPorts
 from acq400 import Acq2106
 from rad_dds import RAD3DDS
 from shotcontrol import ShotController, ActionScript
