92d19b73213da2a8ce2e47dbd6f1e5a9b0ad789f
Unknown <sean.alsop@d-tacq.co.uk>
2018-05-29 10:14:36 +0100

    Added --help strings to docstings for acq1001 apps

--- user_apps/acq1001/run_livetop.py
+++ user_apps/acq1001/run_livetop.py
@@ -1,5 +1,32 @@
 #!/usr/bin/env python
-# run a livetop process
+"""
+run a livetop process
+
+usage: run_gpg.py [-h] [--trg TRG] [--clk CLK] [--mode MODE]
+                  [--disable DISABLE] [--stl STL] [--waterfall WATERFALL]
+                  [--trace TRACE] [--hdmi_master HDMI_MASTER]
+                  uut
+
+run_gpg
+
+positional arguments:
+  uut                   uut
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --trg TRG             trigger fp|soft|softloop|softonce
+  --clk CLK             clk int|dX|notouch
+  --mode MODE           mode
+  --disable DISABLE     1: disable
+  --stl STL             stl file
+  --waterfall WATERFALL
+                        d0,d1,d2,d3 waterfall [interval,hitime]
+  --trace TRACE         trace wire protocol
+  --hdmi_master HDMI_MASTER
+                        clk, trg and gpg drive HDMI outputs
+"""
+
+
 import acq400_hapi
 import argparse
 
