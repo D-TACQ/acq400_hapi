1900b7e4d604de0010f19555886f4e47ef034fd3
Peter Milne <peter.milne@d-tacq.com>
2018-08-09 20:29:18 +0100

    introduce intSI_cvt(), standalone converter 	modified:   acq400_hapi/__init__.py 	modified:   acq400_hapi/intSIAction.py

--- acq400_hapi/intSIAction.py
+++ acq400_hapi/intSIAction.py
@@ -1,28 +1,28 @@
 import sys
 import argparse
 
+def intSI_cvt(value, decimal=True):
+    x = str(value)
+    if x == "Inf":
+        return sys.maxint
+    units = x.find('M')
+    if units >= 0:
+        return int(x[0:units]) * (1000000 if decimal else 0x100000)
+    else:
+        units = x.find('k')
+        if units >= 0:
+            return int(x[0:units]) * (1000 if decimal else 0x400)
+        else:
+            return int(x)
+
 class intSIAction(argparse.Action):
     def __init__ (self, option_strings, decimal=True, *args, **kwargs):
         super(intSIAction, self).__init__(option_strings=option_strings,
                 *args, **kwargs)
         self.decimal = decimal
 
-    def intSI(self, value):
-        x = str(value)
-        if x == "Inf":
-            return sys.maxint
-        units = x.find('M')
-        if units >= 0:
-            return int(x[0:units]) * (1000000 if self.decimal else 0x100000)
-        else:
-            units = x.find('k')
-            if units >= 0:
-                return int(x[0:units]) * (1000 if self.decimal else 0x400)
-            else:
-                return int(x)
-
     def __call__(self, parser, args, value, option_string=None):
-       setattr(args, self.dest, self.intSI(value))
+       setattr(args, self.dest, intSI_cvt(value, decimal=self.decimal))
         
 
 # unit test
