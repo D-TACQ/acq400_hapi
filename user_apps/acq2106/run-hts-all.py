b6842cb80d45d8c1697040c43084f2dce8271eb0
Peter Milne <peter.milne@d-tacq.com>
2018-11-08 22:59:44 +0000

    with example called program 	modified:   user_apps/acq2106/run-hts-all.py

--- user_apps/acq2106/run-hts-all.py
+++ user_apps/acq2106/run-hts-all.py
@@ -1,6 +1,22 @@
 #!/usr/bin/python
 
-from subprocess import call
+import argparse
+import subprocess
 
-call(['mate-terminal', '--window-with-profile=Default', '--title="pgmwashere"',
-       '-e', 'sleep 100'])
+
+def run_shot(args):
+    subprocess.check_call(['mate-terminal', '--window-with-profile=Default', \
+            '--title="pgmwashere"',\
+           '-e', 'timeout {} top'.format(args.secs)])
+
+
+def run_main():
+    parser = argparse.ArgumentParser(description='run hts all uuts')
+    parser.add_argument('--secs', default=100, help='seconds to run')
+    parser.add_argument('uut', nargs='+', help='uut')
+    run_shot(parser.parse_args())
+
+# execution starts here
+
+if __name__ == '__main__':
+    run_main()
