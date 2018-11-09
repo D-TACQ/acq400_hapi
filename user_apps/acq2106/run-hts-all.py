55a29ffecc25a1e13e3274c0d911f2abfaed65c3
Peter Milne <peter.milne@d-tacq.com>
2018-11-09 22:33:51 +0000

    does get-ident lookup, automates hts from list of uuts 	modified:   user_apps/acq2106/run-hts-all.py

--- user_apps/acq2106/run-hts-all.py
+++ user_apps/acq2106/run-hts-all.py
@@ -4,10 +4,40 @@ import argparse
 import subprocess
 
 
+
+class Struct(object):
+    def __init__(self, **kwds):
+        self.__dict__.update(kwds)
+
+def map_uuts():
+    ids = subprocess.check_output(["get-ident-all"]).split('\n')
+    uut_port_map = {}
+    maps = [ id.split(' ') for id in ids ]
+    for map in maps:
+        if len(map) == 4 and map[2].startswith('acq2106'):
+            s = Struct(uut=map[2], lport=map[1], rport=map[3])
+            uut_port_map[s.uut] = s
+
+    return uut_port_map
+
+uuts = map_uuts()
+
+
 def run_shot(args):
-    subprocess.check_call(['mate-terminal', '--window-with-profile=Default', \
-            '--title="pgmwashere"',\
-           '-e', 'timeout {} top'.format(args.secs)])
+    cmd = ['mate-terminal']
+    tabdef = '--window-with-profile=Default'
+
+    for uut in args.uut:
+        ports = uuts[uut]
+        cmd.append(tabdef)
+        cmd.append('--title={}'.format(uut))
+        cmd.append('--command=run-hts {} {} {} {}'.\
+                format(uut, ports.lport, args.secs, ports.rport))
+                
+        tabdef = '--tab-with-profile=Default'
+
+    print(cmd)
+    subprocess.check_call(cmd)
 
 
 def run_main():
