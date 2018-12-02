35d1f2612ba7cf6d1f15c92e97dae9f2d2bde670
Peter Milne <peter.milne@d-tacq.com>
2018-12-02 15:10:20 +0000

    threading rools OK: [pgm@hoy5 acq400_hapi]$ time ./user_apps/acq400/acq400_reset_counters.py  --threaded=0 --sites=s0,cA acq2106_130 acq2106_131 acq2106_132 acq2106_133 Acq2106 acq2106_130 Acq2106 acq2106_131 Acq2106 acq2106_132 Acq2106 acq2106_133 ['s0', 'cA'] reset acq2106_130 s0 done reset acq2106_130 cA done reset acq2106_131 s0 done reset acq2106_131 cA done reset acq2106_132 s0 done reset acq2106_132 cA done reset acq2106_133 s0 done reset acq2106_133 cA done
    real	0m20.233s
    
    [pgm@hoy5 acq400_hapi]$ time ./user_apps/acq400/acq400_reset_counters.py  --sites=s0,cA acq2106_130 acq2106_131 acq2106_132 acq2106_133
    Acq2106 acq2106_130
    Acq2106 acq2106_131
    Acq2106 acq2106_132
    Acq2106 acq2106_133
    ['s0', 'cA']
    reset acq2106_130 cA done
    reset acq2106_131 cA done
    reset acq2106_132 cA done
    reset acq2106_133 cA done
    reset acq2106_131 s0 done
    reset acq2106_130 s0 done
    reset acq2106_133 s0 done
    reset acq2106_132 s0 done
    
    real	0m7.957s

--- user_apps/acq400/acq400_reset_counters.py
+++ user_apps/acq400/acq400_reset_counters.py
@@ -16,19 +16,48 @@ optional arguments:
 import sys
 import acq400_hapi
 import argparse
-        
-def reset_counters(args):
+import threading
+
+def reset1(uut, s):
+    uut.svc[s].RESET_CTR = '1'
+    print "reset {} {} done".format(uut.uut, s)
+
+def reset_counters_threaded(args):
     uuts = [acq400_hapi.Acq2106(u) for u in args.uuts]
     sites = args.sites.split(',')
     print sites
+    threads = []
+    for uut in uuts:
+        for s in sites:                       
+            t = threading.Thread(target=reset1, args=(uut, s))
+            threads.append(t)
+            t.start()
+             
+    for t in threads:
+        t.join()
+            
+            
+def reset_counters_serial(args):
+    uuts = [acq400_hapi.Acq2106(u) for u in args.uuts]
+    sites = args.sites.split(',')
+    print sites
+    
     for uut in uuts:
         for s in sites:
-            uut.svc[s].RESET_CTR = '1'
+            reset1(uut, s)
+    
+  
+def reset_counters(args):
+    if args.threaded:     
+        reset_counters_threaded(args)
+    else:
+        reset_counters_serial(args)
 
 def run_main():
     parser = argparse.ArgumentParser(description='acq400_reset_counters')
     parser.add_argument('-s','--sites', default='s0', help="sites to clear eg s0,s1,s2,cA") 
     parser.add_argument('-t','--trace', default=0, help="traces command execution")
+    parser.add_argument('--threaded', type=int, default=1)
     parser.add_argument('uuts', nargs='+', help="uut[s]")
     reset_counters(parser.parse_args())
 
