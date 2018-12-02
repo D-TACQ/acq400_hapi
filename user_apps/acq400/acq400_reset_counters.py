117983a05ca8e1a0e271593b20b1a3fd3965dd14
Peter Milne <peter.milne@d-tacq.com>
2018-12-02 14:50:37 +0000

    bug fix 	modified:   user_apps/acq400/acq400_reset_counters.py

--- user_apps/acq400/acq400_reset_counters.py
+++ user_apps/acq400/acq400_reset_counters.py
@@ -26,11 +26,11 @@ def reset_counters(args):
             uut.svc[s].RESET_CTR = '1'
 
 def run_main():
-    parser = argparse.ArgumentParser(description='acq400_remote_script')
+    parser = argparse.ArgumentParser(description='acq400_reset_counters')
     parser.add_argument('-s','--sites', default='s0', help="sites to clear eg s0,s1,s2,cA") 
     parser.add_argument('-t','--trace', default=0, help="traces command execution")
     parser.add_argument('uuts', nargs='+', help="uut[s]")
-    remote_script(parser.parse_args())
+    reset_counters(parser.parse_args())
 
 
 # execution starts here
