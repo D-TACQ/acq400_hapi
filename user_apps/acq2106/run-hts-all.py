81ab57705cbb66fa9febea9a65f7f032cbbb7a3d
Peter Milne <peter.milne@d-tacq.com>
2018-12-07 12:04:41 +0000

    make clear the difference between UUTS : global array of UUT instances and args.uutnames : args array of uut names 	modified:   user_apps/acq2106/run-hts-all.py

--- user_apps/acq2106/run-hts-all.py
+++ user_apps/acq2106/run-hts-all.py
@@ -26,12 +26,12 @@ def map_uuts():
 
     return uut_port_map
 
-uuts = []
+UUTS = []
 
 
 def make_fifos(args):
     args.fifos = []
-    for uut in args.uuts:
+    for uut in args.uutnames:
         fn = "/tmp/{}.hts".format(uut)
         try:
             os.mkfifo(fn)            
@@ -53,7 +53,7 @@ def set_sync_roles(args):
         toprole += ',fptrg'
     cmd.append('--toprole={}'.format(toprole))
     cmd.append('--fclk={}'.format(args.fclk))
-    for uut in args.uuts:
+    for uut in args.uutnames:
         cmd.append(uut)
     print(cmd)
     subprocess.check_call(cmd)        
@@ -64,7 +64,7 @@ def wait_for_state(args, state, timeout=0):
     global time0
     if time0 == 0:
         time0 = time.time()  
-    for uut in uuts:
+    for uut in UUTS:
         olds = ""
         finished = False
         dots = 0
@@ -96,25 +96,25 @@ def release_the_trigger(args):
     cmd = []
     cmd.extend(scmd)
     cmd.append('--enable_trigger=1')
-    cmd.append(args.uuts[0])
+    cmd.append(args.uutnames[0])
     print(cmd)
     subprocess.check_call(cmd)     
 
 def init_shot(args):
-    global uuts
+    global UUTS
     global uut_port_map
 
     if args.run_hts:
         uut_port_map = map_uuts()
         make_fifos(args)
-    uuts = [acq400_hapi.Acq400(u) for u in args.uuts]
+    UUTS = [acq400_hapi.Acq400(u) for u in args.uutnames]
 
-    for uut in uuts:
+    for uut in UUTS:
         uut.s0.transient = "SOFT_TRIGGER=0"
 
     if acq400_hapi.intSI_cvt(args.fclk) > 40000000:
         print("fclk > 40M, setting 2x4 mode")
-        for uut in uuts:
+        for uut in UUTS:
             uut.s0.stack_480 = '2x4'
 
     set_sync_roles(args)
@@ -146,12 +146,12 @@ def _store_shot(shot):
 
 def store_shot(args):
     wait_for_state(args, 'IDLE')
-    shot = [ u.s1.shot for u in uuts]
+    shot = [ u.s1.shot for u in UUTS]
 
     s0 = shot[0]
     for ii, s1 in enumerate(shot[1:]):
         if s0 != s1:
-            print("WARNING: uut {} shot {} does not equal master {}".format(uuts[ii].uut, s1, s0))
+            print("WARNING: uut {} shot {} does not equal master {}".format(UUTS[ii].uut, s1, s0))
 
     _store_shot(int(shot[0]))
 
@@ -159,7 +159,7 @@ def run_hts(args):
     cmd = ['mate-terminal']
     tabdef = '--window-with-profile=Default'
 
-    for ii, uut in enumerate(args.uuts):
+    for ii, uut in enumerate(args.uutnames):
         ports = uut_port_map[uut]
         cmd.append(tabdef)
         cmd.append('--title={}'.format(uut))
@@ -175,12 +175,12 @@ def run_hts(args):
     subprocess.check_call(cmd)    
 
 def print_mgt_command(args):
-    global uuts
+    global UUTS
     BLOCKSZ = 0x400000
-    nchan = int(uuts[0].s0.NCHAN)
+    nchan = int(UUTS[0].s0.NCHAN)
     mbps = nchan * acq400_hapi.intSI_cvt(args.fclk) * 2
     blocks = mbps * int(args.secs) / BLOCKSZ
-    for uut in args.uuts:
+    for uut in args.uutnames:
         print("# python mgtdramshot.py --captureblocks={} {}".format(blocks, uut))
 
 # sudo mv /mnt/datastream/ACQ400DATA/* /mnt/datastream/SHOT_0134/    
@@ -207,7 +207,7 @@ def run_main():
     parser.add_argument('--fclk', type=str, default='10M', help='sample clock before decimation')
     parser.add_argument('--store', type=int, default=0, help='1: store shot after capture')
     parser.add_argument('--run_hts', type=int, default=1, help='1: run the hts-helper, 0: do not run')
-    parser.add_argument('uuts', nargs='+', help='uut')
+    parser.add_argument('uutnames', nargs='+', help='uut')
     args = parser.parse_args()
     init_shot(args)
     run_shot(args)
