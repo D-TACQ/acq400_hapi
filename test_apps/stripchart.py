d4e99a5780f91b3ef40ef32c914a379dc3c81f85
seanalsop <sean.alsop@d-tacq.co.uk>
2018-12-21 15:14:26 +0000

    Added cli args to stripchart. 	modified:   stripchart.py

--- test_apps/stripchart.py
+++ test_apps/stripchart.py
@@ -1,35 +1,50 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import argparse
 import time
 import subprocess
 
-plt.ion()
-dlist = []
+def stripchart(args):
 
-for item in range(0, 8):
-    dlist.append([])
+    plt.ion()
+    dlist = []
+    print args.chs
+    for item in range(0, args.chs):
+        dlist.append([])
 
-while True:
-    data = subprocess.check_output(["./dumpshm.ai"])
-    data = data.replace(" ", "")
-    data = data.split(",")
-    data = data[:-1]
-    print "data = ", data
+    while True:
+        data = subprocess.check_output(["./dumpshm.ai"])
+        data = data.replace(" ", "")
+        data = data.split(",")
+        data = data[:-1]
+        print "data = ", data
 
-    for num, list in enumerate(dlist):
-        dlist[num].append(data[num])
-
-    for num, list in enumerate(dlist):
-        plt.plot(dlist[num])
-
-
-    plt.draw()
-    plt.pause(0.0001)
-    plt.clf()
-    print "drawing"
-    if len(dlist[0]) > 150:
         for num, list in enumerate(dlist):
-            dlist[num] = dlist[num][1:-1]
-    time.sleep(1)
-    num+=1
+            dlist[num].append(data[num])
+            #print "list ", num, " = ", dlist[num]
+
+        for num, list in enumerate(dlist):
+            plt.plot(dlist[num])
+
+        if len(dlist[0]) > 75:
+            for num, list in enumerate(dlist):
+                dlist[num] = dlist[num][1:-1]
+
+        #plt.ylim((-100,100))
+        plt.draw()
+        plt.pause(0.1)
+        plt.clf()
+        print "drawing"
+
+        time.sleep(args.sleep)
+
+def run_main():
+    parser = argparse.ArgumentParser(description='acq400 stream')
+    parser.add_argument('--sleep', default=1, type=int, help="How long to sleep between stripchart loads")
+    parser.add_argument('--chs', default=4, type=int, help="How many channels to plot.")
+    args = parser.parse_args()
+    stripchart(args)
+
+if __name__ == '__main__':
+    run_main()
