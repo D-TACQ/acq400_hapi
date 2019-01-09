e6d441b65e47a3a556bb6cc6cb75c1a42ca2fd3e
Peter Milne brotto <peter.milne@d-tacq.com>
2019-01-09 05:51:38 +0000

    --awgsite=N .. set master site for AWG (default 1) FUTURE: determine automatically when s1.dist_s1 is a knob 	modified:   user_apps/special/awg_oneshot_speed_test.py

--- user_apps/special/awg_oneshot_speed_test.py
+++ user_apps/special/awg_oneshot_speed_test.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 import argparse
 import os
 import time
@@ -41,15 +42,15 @@ def _run_shot_py(uut, fn, port):
 
 _run_shot = _run_shot_nc
 
-def run_shot(uut, shot, fn, port):
+def run_shot(uut, shot, fn, port, awgsite):
     print("run_shot {} {}".format(shot, fn))
 #    uut.s1.playloop_length = '0 0'
-    uut.s1.playloop_maxlen = os.stat(fn).st_size/SAMPLESZ
+    awgsite.playloop_maxlen = os.stat(fn).st_size/SAMPLESZ
     t1 = time.time()
     _run_shot(uut, fn, port)
     t2 = time.time()
     uut.s0.soft_trigger = '1'
-    while uut.s1.task_active == '1':
+    while awgsite.task_active == '1':
         time.sleep(0.1)
     t3 = time.time()
     return( t1, t2, t3)
@@ -61,12 +62,14 @@ def run_test(args):
         _run_shot = _run_shot_py
     port = 54203 if args.overlap_load else 54201
     uut = acq400_hapi.Acq400(args.uut)
-    uut.s1.shot = '0'
-    uut.s1.completed_shot = '0'
+    awgsite = uut.svc["s{}".format(args.awgsite)]
+
+    awgsite.shot = '0'
+    awgsite.completed_shot = '0'
     times = []
     for shot in range(0, args.reps):
         for fn in args.files:
-            times.append(run_shot(uut, shot, fn, port))
+            times.append(run_shot(uut, shot, fn, port, awgsite))
 	    if args.gaps:
 		time.sleep(args.gaps)
     
@@ -83,6 +86,7 @@ def run_main():
     parser = argparse.ArgumentParser(description = 'awg speed test')
     parser.add_argument('--reps', type=int, default=1, help='number of repetitions')
     parser.add_argument('--uut',  default=None, help='uut')
+    parser.add_argument('--awgsite', default=1, help="first awg site")
     parser.add_argument('--gaps', default=0, type=int, help='gap in s between shots')
     parser.add_argument('--overlap_load', default=1, type=int)
     parser.add_argument('--pure_python', default=0, type=int)
