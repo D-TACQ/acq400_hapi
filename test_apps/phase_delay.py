e6c267a76cb37cf1ce8600212108596a8b3ed709
seanalsop <sean.alsop@d-tacq.co.uk>
2019-01-14 15:49:56 +0000

    Added new methods of detecting phase delay. 	modified:   phase_delay.py

--- test_apps/phase_delay.py
+++ test_apps/phase_delay.py
@@ -3,6 +3,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import argparse
+from scipy.signal import hilbert, chirp
 
 def run_test(args):
 
@@ -23,14 +24,39 @@ def run_test(args):
 
     #Difference
     y3 = (x2 - x1) / m1
-    y3_mean = np.mean(abs(np.subtract(x2,x1)) / m1)
 
-    print "Phase difference in rads = ", y3_mean
+    if args.method == 1:
+        # Using 'Taylor Series' method
+        phase_diff = np.mean(abs(np.subtract(x2,x1)) / m1)
+
+    elif args.method == 2:
+        # Using phase sensitive detector
+        phase_diff = np.arccos( np.dot(x1,x2) / (np.linalg.norm(x1)*np.linalg.norm(x2)) )
+
+    elif args.method == 3:
+        # FFT method
+        fft_x1 = np.fft.fft(x1)
+        fft_x2 = np.fft.fft(x2)
+        print "ffts = ", fft_x1, fft_x2
+        l1 = len(fft_x1)
+        l2 = len(fft_x2)
+        phase_diff = np.angle(fft_x1[0:l1+1/2] / fft_x2[0:l2+1/2])
+        phase_diff = float(np.pi) - float(phase_diff[0])
+
+    elif args.method == 4:
+        # Hilbert transform method
+        x1h = hilbert(x1)
+        x2h = hilbert(x2)
+        ph_diff = np.angle(x1h/x2h)
+        phase_diff = ph_diff[0]
+
+    print "Phase difference in rads = ", phase_diff
 
 def run_main():
     parser = argparse.ArgumentParser(description = 'awg speed test')
     parser.add_argument('--file1', type=str, default="CH01", help='ch1 file name')
     parser.add_argument('--file2', type=str, default="CH02", help='ch2 file name')
+    parser.add_argument('--method', type=int, default=1, help='Which method to use.')
     run_test(parser.parse_args())
 
 
