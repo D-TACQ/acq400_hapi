76f1560558fd9a38e6bd097f9d93c27b0e8ae4ea
Sean Alsop <sean.alsop@d-tacq.co.uk>
2019-01-23 11:48:48 +0000

    Added wait_for_state to shotcontrol and changed sync_trg_test to use shotcontrol version.

--- test_apps/sync_trg_test.py
+++ test_apps/sync_trg_test.py
@@ -11,37 +11,6 @@ import time
 import sys
 import argparse
 
-def wait_for_state(uut, state, timeout=0):
-    UUTS = [uut]
-    time0 = 0
-    if time0 == 0:
-        time0 = time.time()
-    for uut in UUTS:
-        olds = ""
-        finished = False
-        dots = 0
-        pollcat = 0
-
-        while not finished:
-            st = uut.s0.TRANS_ACT_STATE.split(' ')[1] #Real name TRANS_ACT:STATE
-            finished = st == state
-            news = "polling {}:{} {} waiting for {}".format(uut.uut, st, 'DONE' if finished else '', state)
-            if news != olds:
-                sys.stdout.write("\n{:06.2f}: {}".format(time.time() - time0, news))
-                olds = news
-            else:
-                sys.stdout.write('.')
-                dots += 1
-                if dots >= 20:
-                    dots = 0
-                    olds = ""
-            if not finished:
-                if timeout and (time.time() - time0) > timeout:
-                    sys.exit("\ntimeout waiting for {}".format(news))
-                time.sleep(1)
-            pollcat += 1
-    print("")
-
 
 def run_test(args):
 
@@ -58,7 +27,7 @@ def run_test(args):
         ### Arm uuts
         for uut in args.uuts:
             uut.s0.set_arm = 1
-            wait_for_state(uut, "ARM") # Pass hostname and required state
+            acq400_hapi.shotcontrol.wait_for_state(uut, "ARM") # Pass hostname and required state
         print "Set arm"
 
         ### Change trigger back to external on master
@@ -67,7 +36,7 @@ def run_test(args):
 
         # Wait for UUTs to be IDLE again
         for uut in args.uuts:
-            wait_for_state(uut, "IDLE") # Pass hostname and required state
+            acq400_hapi.shotcontrol.wait_for_state(uut, "IDLE") # Pass hostname and required state
 
         # Check shot number across all UUTs
         shotnums = []
