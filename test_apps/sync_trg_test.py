7a61621fd8da481b793cfdae5918dc8e52e6a4b5
Sean Alsop <sean.alsop@d-tacq.co.uk>
2019-01-23 11:39:10 +0000

    Extended sync_trg_test.py to N uuts + more. 	modified:   test_apps/sync_trg_test.py         Extended sync_trg_test.py to N uuts and added         shot number and sample count tests as well.

--- test_apps/sync_trg_test.py
+++ test_apps/sync_trg_test.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python
 
+"""
+A script used to test N UUTs syncd using sync_role. This script assumes this has
+already been performed. A capture is performed and the
+"""
+
+
 import acq400_hapi
 import time
 import sys
-
+import argparse
 
 def wait_for_state(uut, state, timeout=0):
     UUTS = [uut]
@@ -37,29 +43,64 @@ def wait_for_state(uut, state, timeout=0):
     print("")
 
 
-### Set up UUTs
-uut1 = acq400_hapi.Acq400(sys.argv[1]) # first arg
-uut2 = acq400_hapi.Acq400(sys.argv[2]) # second arg
+def run_test(args):
 
-while True:
+    ### Set up UUTs
+    for num, uut in enumerate(args.uuts):
+        args.uuts[num] = acq400_hapi.Acq400(uut)
 
-    ### Change trg to d4
-    uut1.s1.trg = "1,5,1"
-    print "Setting trg to no trg"
+    while True:
 
-    ### Arm uuts
-    uut2.s0.set_arm = 1
-    wait_for_state(uut2, "ARM")
-    uut1.s0.set_arm = 1
-    wait_for_state(uut1, "ARM")
-    print "Set arm"
+        ### Change trg to d4 on master
+        args.uuts[0].s1.trg = "1,5,1"
+        print "Setting trg to no trg"
 
-    ### Change trigger back to external
-    uut1.s1.trg = "1,0,1"
-    print "Set trigger to ext"
+        ### Arm uuts
+        for uut in args.uuts:
+            uut.s0.set_arm = 1
+            wait_for_state(uut, "ARM") # Pass hostname and required state
+        print "Set arm"
 
-    # Wait for UUTs to be IDLE again
-    wait_for_state(uut1, "IDLE")
-    wait_for_state(uut2, "IDLE")
+        ### Change trigger back to external on master
+        args.uuts[0].s1.trg = "1,0,1"
+        print "Set trigger to ext"
 
-    
+        # Wait for UUTs to be IDLE again
+        for uut in args.uuts:
+            wait_for_state(uut, "IDLE") # Pass hostname and required state
+
+        # Check shot number across all UUTs
+        shotnums = []
+        for uut in args.uuts:
+            shotnums.append(uut.s1.shot)
+
+        if shotnums[1:] == shotnums[:-1]: # Check every element in list is identical
+            print "Shot numbers agree. Success."
+        else:
+            print "Shot numbers do not agree. Fail."
+
+        # Check all UUTs captured the correct number of samples
+        samples = [] # store all "number of samples taken during shot" in list
+        for uut in args.uuts:
+            samples.append(uut.s0.TRANS_ACT_POST) # number of post shot samples
+
+        if samples[1:] == samples[:-1]:
+            print "# of samples recorded by each UUT are identical. Success."
+        else:
+            print "# of samples recorded by each UUT not identical. Fail."
+            print "Ending test now"
+            break
+
+        print "Finished iteration"
+
+
+def run_main():
+    parser = argparse.ArgumentParser(description='synchronised trigger test')
+    parser.add_argument('uuts', nargs='+', help='UUTs - master should be first')
+    args = parser.parse_args()
+    run_test(args)
+
+
+
+if __name__ == '__main__':
+    run_main()
