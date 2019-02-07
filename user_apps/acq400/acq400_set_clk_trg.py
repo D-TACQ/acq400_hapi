44eb2341c3ba11032c71480f00d000bb33459d6e
Sean Alsop <sean.alsop@d-tacq.co.uk>
2019-02-07 16:11:30 +0000

    Changed RADCELF to be more like sync_role. 	modified:   acq400_set_clk_trg.py

--- user_apps/acq400/acq400_set_clk_trg.py
+++ user_apps/acq400/acq400_set_clk_trg.py
@@ -34,20 +34,29 @@ def set_clk_trg(args):
         uut.s1.trg = "1,",args.trg,",1"
         uut.s1.sync = "0,0,0"
         uut.s1.clkdiv = 1
+        uut.s1.sync_trg_to_clk=1
 
     else:
         print "Configuring as slave"
-        uut.s0.SIG_SRC_CLK_0 = "HDMI"
+        uut.s0.SIG_SRC_CLK_0 = "EXT"
+        uut.s0.SIG_SRC_CLK_1 = "HDMI"
         uut.s0.SIG_SRC_TRG_0 = "HDMI"
         uut.s0.SIG_SRC_SYNC_0 = "HDMI"
+        uut.s0.SIG_SYNC_OUT_CLK = "CLK"
+        uut.s0.SIG_SYNC_OUT_CLK_DX = "d1"
+        uut.s0.SIG_SYNC_OUT_TRG = "TRG"
+        uut.s0.SIG_SYNC_OUT_TRG_DX = "d0"
+        uut.s0.SIG_SYNC_OUT_SYNC = "SYNC"
+        uut.s0.SIG_SYNC_OUT_SYNC_DX = "d0"
         uut.s1.clk = "1,0,1"
         uut.s1.trg = "1,0,1"
         uut.s1.sync = "1,0,1"
+        uut.s1.sync_trg_to_clk=0
 
 
 def run_main():
     parser = argparse.ArgumentParser(description = 'set clk trg')
-    parser.add_argument('--trg', type=str, default="d1",
+    parser.add_argument('--trg', type=str, default="d2",
     help="Which trigger to use. Default is soft.")
     parser.add_argument('--uut', type=str, default=socket.gethostname(),
     help='Which UUT we are working with. Default is found using socket.')
