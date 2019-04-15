9cf0053a2124fb4a3949c8ee979b490bb1d0dedc
Sean Alsop <sean.alsop@d-tacq.co.uk>
2019-04-15 12:26:47 +0100

    Changed pre_post default trigger to ext.

--- acq400_hapi/acq400.py
+++ acq400_hapi/acq400.py
@@ -618,7 +618,7 @@ class Acq400:
             nc.sock.close()
 
 
-    def configure_pre_post(self, role, trigger="int", pre=50000, post=100000):
+    def configure_pre_post(self, role, trigger="ext", pre=50000, post=100000):
         # configure UUT for pre/post mode. Default: soft trigger starts the
         # data flow and we trigger the event on a hard external trigger.
         if pre > post:
