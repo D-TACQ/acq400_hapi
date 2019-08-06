abe3e828929797d2ee395b9d6eda65b29eff7761
Sean <sean.alsop@d-tacq.co.uk>
2019-08-06 09:55:56 +0100

    Added n-trg-abort.py. 	new file:   n-trg-abort.py

---
+++ user_apps/acq400/n-trg-abort.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+
+"""
+n-trg-abort.py is a script that monitors the number of triggers that the UUT
+recieves and sends the abort command once n triggers have passed.
+
+The script is intended to be run before starting a stream. For example, if the
+user wanted to start the stream on one trigger and stop the stream on the next
+trigger, then the user would set --n=2 as such:
+
+./n-trg-abort.py --n=2 --reset=1 acq2106_085
+
+The reset parameter shown above sets the counter to 0
+"""
+
+
+import acq400_hapi
+import numpy as np
+import matplotlib.pyplot as plt
+import argparse
+
+
+def run(args):
+
+    uut = acq400_hapi.Acq400(args.uut[0])
+
+    if args.reset == 1:
+        uut.s0.SIG_TRG_EXT_RESET = 1
+        while int(uut.s0.SIG_TRG_EXT_COUNT.split(" ")[1]) != 0:
+            continue
+
+
+    current_trg = int(uut.s0.SIG_TRG_EXT_COUNT.split(" ")[1])
+    end_trg = current_trg + args.n
+
+    while current_trg < end_trg:
+        current_trg = int(uut.s0.SIG_TRG_EXT_COUNT.split(" ")[1])
+        print("Looped. Current = {}, End = {}".format(current_trg, end_trg))
+
+    uut.s0.set_abort = 1
+
+    return None
+
+
+def main():
+    parser = argparse.ArgumentParser(description='stop after n triggers')
+
+    parser.add_argument('--n', default=2, type=int,
+    help="How many trgs to wait through before sending abort cmd. Default: 2")
+
+    parser.add_argument('--reset', default=0, type=int,
+    help='Whether or not to reset the trg counter before starting. Default: 0')
+
+    parser.add_argument('uut', nargs=1, help="uut ")
+
+    run(parser.parse_args())
+
+
+if __name__ == '__main__':
+    main()
