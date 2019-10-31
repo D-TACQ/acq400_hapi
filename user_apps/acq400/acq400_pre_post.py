98f9f1030aece6b9d18243e4bb6d28f1fe18f8c4
Peter Milne <peter.milne@d-tacq.com>
2019-10-31 14:35:59 +0000

    split functions to each do just one thing BF: reset data[] every shot rename --loops to --shots 	modified:   user_apps/acq400/acq400_pre_post.py

--- user_apps/acq400/acq400_pre_post.py
+++ user_apps/acq400/acq400_pre_post.py
@@ -11,7 +11,7 @@ trigger is given (~0.5Hz) with CH01 sampling a square wave.
 Example usage:
 
 python3 acq400_pre_post.py --data_test=1 --pre=50000 --post=100000 \
---trg=ext --loops=5 acq1001_084
+--trg=ext --shots=5 acq1001_084
 """
 
 
@@ -24,52 +24,60 @@ import matplotlib.pyplot as plt
 import sync_role
 
 
-def run_shot(args):
-    data = []
+def validate(args, data, shot):
+# for ch in data[0]:
+    ch = data[0][0]
+    zero_crossings = np.where(np.diff(np.sign(ch)))[0]
+    test_range = range(args.pre - 5, args.pre + 5)
+    
+    for crossing in test_range:
+        if crossing in zero_crossings:
+            print("{} Test passed - pre/post appears to be working.".format(shot))
+            break
+        elif crossing != test_range[-1]:
+            continue
+        else:
+            print("{} Test failed".format(shot))
+            plt.plot(ch)
+            plt.show()
+
+def configure_shot(args):
     configure_shot(args)
     sync_role.run_shot(args)
     uuts = [acq400_hapi.Acq400(u) for u in args.uuts]
 
     for uut in uuts:
         uut.s0.transient = "DEMUX=1"
+        
+    return uuts
 
-    for loop in range(0, args.loops):
+def run_shot(args, uuts, shot):
+    for uut in reversed(uuts):
+        uut.s0.set_arm
+        uut.statmon.wait_armed()
+    data = []
+    
+    for uut in uuts:
+        uut.statmon.wait_stopped()
+        data.append(uut.read_channels((1)))
 
-        for uut in reversed(uuts):
-            uut.s0.set_arm
-            uut.statmon.wait_armed()
+    if args.data_test == 1:
+        validate(args, data, shot)    
 
-        for uut in uuts:
-            uut.statmon.wait_stopped()
-            data.append(uut.read_channels((1)))
+    
+def run_shots(args, uuts):
+    for shot in range(0, args.shots):
+        run_shot(args, uuts, shot)
 
-        if args.data_test == 1:
-
-            # for ch in data[0]:
-            ch = data[0][0]
-            zero_crossings = np.where(np.diff(np.sign(ch)))[0]
-            test_range = range(args.pre - 5, args.pre + 5)
-            for crossing in test_range:
-                if crossing in zero_crossings:
-                    print("Test passed - pre/post appears to be working.")
-                    break
-                elif crossing != test_range[-1]:
-                    continue
-                else:
-                    print("Test failed")
-                    plt.plot(ch)
-                    plt.show()
-
-    return None
 
 
 def main():
     parser = argparse.ArgumentParser(description='run pre-post capture.')
     acq400_hapi.Acq400UI.add_args(parser)
     parser.add_argument('--data_test', type=int, default=0,
-    help='Whether to test the data for the trigger at the pre/post crossover')
-    parser.add_argument('--loops', type=int, default=1,
-    help='The number of pre-post captures to run.')
+                        help='Whether to test the data for the trigger at the pre/post crossover')
+    parser.add_argument('--shots', type=int, default=1,
+                        help='The number of pre-post captures to run.')
 
     parser.add_argument('--enable_trigger', default=None, help="set this to enable the trigger all other args ignored")
     parser.add_argument('--toprole', default='master', help="role of top in stack")
@@ -78,7 +86,9 @@ def main():
     parser.add_argument('--clkdiv', default=None, help="optional clockdiv")
 
     parser.add_argument('uuts', nargs='+', help='uut1 [uut2..]')
-    run_shot(parser.parse_args())
+    
+    args = parser.parse_args()
+    run_shots(args, configure_shot(args))
 
 
 if __name__ == '__main__':
