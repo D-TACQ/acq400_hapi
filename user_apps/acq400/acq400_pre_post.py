ecc46978f6046d98ab0d8ef3eeafe654dd2bd2fc
Sean <sean.alsop@d-tacq.co.uk>
2019-10-31 12:27:52 +0000

    Added ability to loop the captures. 	modified:   acq400_pre_post.py

--- user_apps/acq400/acq400_pre_post.py
+++ user_apps/acq400/acq400_pre_post.py
@@ -13,35 +13,33 @@ def run_shot(args):
     configure_shot(args)
     uuts = [acq400_hapi.Acq400(u) for u in args.uuts]
 
-    for uut in reversed(uuts):
-        uut.s0.transient = "DEMUX = 1"
-        uut.s0.set_arm
-        uut.statmon.wait_armed()
+    for loop in range(0, args.loops):
 
-    for uut in uuts:
-        uut.statmon.wait_stopped()
-        data.append(uut.read_channels((1)))
+        for uut in reversed(uuts):
+            uut.s0.transient = "DEMUX = 1"
+            uut.s0.set_arm
+            uut.statmon.wait_armed()
 
-    if args.data_test == 1:
+        for uut in uuts:
+            uut.statmon.wait_stopped()
+            data.append(uut.read_channels((1)))
 
-        # for ch in data[0]:
-        ch = data[0][0]
-        zero_crossings = np.where(np.diff(np.sign(ch)))[0]
-        test_range = range(args.pre - 5, args.pre + 5)
-        for crossing in test_range:
-            if crossing in zero_crossings:
-                print("Test passed - pre/post appears to be working.")
-                exit(0)
-            elif crossing != test_range[-1]:
-                continue
-            else:
-                print("Test failed")
-                plt.plot(ch)
-                plt.show()
+        if args.data_test == 1:
 
-        # if zero_crossings[0] > 49995 and zero_crossings[0] < 50005:
-            # plt.plot(ch)
-            # plt.show()
+            # for ch in data[0]:
+            ch = data[0][0]
+            zero_crossings = np.where(np.diff(np.sign(ch)))[0]
+            test_range = range(args.pre - 5, args.pre + 5)
+            for crossing in test_range:
+                if crossing in zero_crossings:
+                    print("Test passed - pre/post appears to be working.")
+                    break
+                elif crossing != test_range[-1]:
+                    continue
+                else:
+                    print("Test failed")
+                    plt.plot(ch)
+                    plt.show()
 
     return None
 
@@ -51,6 +49,8 @@ def main():
     acq400_hapi.Acq400UI.add_args(parser)
     parser.add_argument('--data_test', type=int, default=0,
     help='Whether to test the data for the trigger at the pre/post crossover')
+    parser.add_argument('--loops', type=int, default=1,
+    help='The number of pre-post captures to run.')
     parser.add_argument('uuts', nargs='+', help='uut1 [uut2..]')
     run_shot(parser.parse_args())
 
