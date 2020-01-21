002b359941e52c1e34c818841514840bc8070466
Sean <sean.alsop@d-tacq.co.uk>
2020-01-21 09:54:31 +0000

    Moved code into functions.

--- test_apps/check_transition.py
+++ test_apps/check_transition.py
@@ -11,6 +11,46 @@ import os
 import argparse
 
 
+def check_transition_data(transition_data, transition_points, uut_data, directory):
+
+    error_count = 0
+    for num, row in enumerate(transition_data):
+        if not np.array_equal(row, transition_data[0,0:]):
+            
+            if args.verbose == 1:
+                print('Quick test failed. Trying tolerance test.')
+
+            # If the transitions are in different places then check the values
+            # of the samples are within the user specified tolerance.
+            tp = int(transition_points[num])
+            # print(tp)
+            if np.allclose(uut_data[num,0:][tp-2:tp+2], uut_data[0,0:][tp-2:tp+2], atol=args.tol, rtol=0):
+                if args.verbose == 1:
+                    print('Tolerance test passed.')
+                continue
+
+            else:
+                
+                print("ERROR FOUND IN DIR: {}".format(directory))
+                print("{} transition: {}".format(args.uuts[0], uut_data[0,0:][tp-2:tp+2]))
+                print("{} transition: {}".format(args.uuts[1], uut_data[num,0:][tp-2:tp+2]))
+                error_count += 1
+    return error_count
+
+
+def detect_transition(transition_data, index):
+    """
+    A function used to find the transition point in a binary array.
+    """
+
+    try:
+        transition_point = np.where(np.roll(transition_data, 1) != transition_data)[0][1]
+    except Exception:
+        transition_point = np.where(np.roll(transition_data, 1) != transition_data)[0][0]
+
+    return transition_point
+
+
 def get_args():
     parser = argparse.ArgumentParser(description='Transition test')
 
@@ -53,39 +93,16 @@ def main(args):
             if transition_data[num,0:].all():
                 print("There was no transition in this data. Skipping shot {} now.".format(directory))
                 break
-            try:
-                transition_points[num] = np.where(np.roll(transition_data[num,0:], 1) != transition_data[num,0:])[0][1]
-            except Exception:
-                print(np.where(np.roll(transition_data[num,0:], 1) != transition_data[num,0:]))
-                print(transition_data[num,0:])
-                transition_points[num] = np.where(np.roll(transition_data[num,0:], 1) != transition_data[num,0:])[0][0]
+
+            transition_points[num] = detect_transition(transition_data[num,0:], num)
 
         if args.verbose == 1:
             for num, uut in enumerate(uuts):
                 print("Dir: {}, {} transition: {}".format(directory, uut, transition_points[num]))
 
         # Check if the transition arrays are equal or not.
-        for num, row in enumerate(transition_data):
-            if not np.array_equal(row, transition_data[0,0:]):
-                
-                if args.verbose == 1:
-                    print('Quick test failed. Trying tolerance test.')
+        error_count += check_transition_data(transition_data, transition_points, uut_data, directory)
 
-                # If the transitions are in different places then check the values
-                # of the samples are within the user specified tolerance.
-                tp = int(transition_points[num])
-                # print(tp)
-                if np.allclose(uut_data[num,0:][tp-2:tp+2], uut_data[0,0:][tp-2:tp+2], atol=args.tol, rtol=0):
-                    if args.verbose == 1:
-                        print('Tolerance test passed.')
-                    continue
-
-                else:
-                    
-                    print("ERROR FOUND IN DIR: {}".format(directory))
-                    print("{} transition: {}".format(args.uuts[0], uut_data[0,0:][tp-2:tp+2]))
-                    print("{} transition: {}".format(args.uuts[1], uut_data[num,0:][tp-2:tp+2]))
-                    error_count += 1
 
     print("Count of non-matching transitions: {}".format(error_count))
     return None
