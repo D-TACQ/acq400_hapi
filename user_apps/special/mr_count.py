9793b5a3d3c686f6a4cf56b50368fc9b10616fd9
Peter Milne <peter.milne@d-tacq.com>
2020-04-19 16:14:26 +0100

    includes count of falling edges 	modified:   special/mr_count.py

--- user_apps/special/mr_count.py
+++ user_apps/special/mr_count.py
@@ -9,6 +9,9 @@ default: works on the ACQ2106, reading /dev/acq400/data/1/01
 import numpy as np
 import argparse
 
+def crossings_nonzero_pos2neg(data):
+    pos = data > 0
+    return len((pos[:-1] & ~pos[1:]).nonzero()[0])
 
 def mr_count(args):
 	ch01 = np.fromfile(args.chfile, dtype=np.int16)
@@ -16,12 +19,16 @@ def mr_count(args):
 	print("{} len {}".format(args.chfile, len(ch01)))
 
 	mm1 = 3
+	i0 = 0
 
 	for ix, raw in enumerate(ch01):
 		mode = raw&3
 		if mode != mm1:
-			print("{:8d} : {:d}=>{:d}".format(ix, mm1, mode))
+			if ix > 0:
+				edges = crossings_nonzero_pos2neg(ch01[i0:ix-1])
+				print("{:8d} : {:d}=>{:d} crossings {}".format(ix, mm1, mode, edges))
 			mm1 = mode
+			i0 = ix
 
 def run_main():
     parser = argparse.ArgumentParser(description='acq2106_mr_count')
