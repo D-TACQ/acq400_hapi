e00b065e1f410ee7971eefca1ea6e7450b6bbf9f
Peter Milne <peter.milne@d-tacq.com>
2020-04-19 15:27:45 +0100

    analyses MR signature in raw data runs by default on ACQ2106, but host operation is possible, simply spec rawfile 	new file:   special/mr_count.py

---
+++ user_apps/special/mr_count.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+"""
+
+scans mr data for MR transitions.
+default: works on the ACQ2106, reading /dev/acq400/data/1/01
+
+"""
+
+import numpy as np
+import argparse
+
+
+def mr_count(args):
+#ch01 = np.fromfile("/tmp/01", dtype=np.int16)
+	ch01 = np.fromfile("/dev/acq400/data/1/01", dtype=np.int16)
+
+	print(len(ch01))
+
+	mm1 = 3
+
+	for ix, raw in enumerate(ch01):
+		mode = raw&3
+		if mode != mm1:
+			print("{:8d} : {:d}=>{:d}".format(ix, mm1, mode))
+			mm1 = mode
+
+def run_main():
+    parser = argparse.ArgumentParser(description='acq2106_mr_count')
+    parser.add_argument('chfile', nargs=1, default="/dev/acq400/data/1/01", help="uuts")
+    mr_count(parser.parse_args())
+
+
+# execution starts here
+
+if __name__ == '__main__':
+    run_main()
