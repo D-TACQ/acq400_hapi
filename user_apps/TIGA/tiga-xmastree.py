b8148f432d36f298bfe3c888cf511fcfe79530d2
Peter Milne <peter.milne@d-tacq.com>
2020-07-26 12:31:03 +0100

    skeleton cascading PG function ... 	modified:   user_apps/TIGA/tiga-xmastree.py

--- user_apps/TIGA/tiga-xmastree.py
+++ user_apps/TIGA/tiga-xmastree.py
@@ -35,21 +35,21 @@ def set_clkout(uut):
             sx.CLKDIV = '10'
             clkout_freqs[site] = 25e6
         if tx == 1:
-            print("run_site {} at output 1MHz")
+            print("run_site {} at output 1MHz".format(site))
             sx.CLK = 'external'
             sx.CLK_DX = 'd1'
             sx.CLK_SENSE = 'rising'
             sx.CLKDIV = '10'
             clkout_freqs[site] = 1e6
         if tx == 2:
-            print("chain site {} from left hand site at output 1kHz")
+            print("chain site {} from left hand site at output 1kHz".format(site))
             sx.CLK = 'external'
             sx.CLK_DX = 'd{}'.format(site-1+1)
             sx.CLK_SENSE = 'rising'
             sx.CLKDIV = '1000'
             clkout_freqs[site] = 1e3
         if tx == 3:
-            print("chain site {} from left hand site at output 1Hz")
+            print("chain site {} from left hand site at output 1Hz".format(site))
             sx.CLK = 'external'
             sx.CLK_DX = 'd{}'.format(site-1+1)
             sx.CLK_SENSE = 'rising'
@@ -64,10 +64,78 @@ def set_clkout(uut):
                             "PASS" if in_tolerance(setpoint, actual) else "FAIL"))
         
         
+def set_trgin(uut): 
+    print("\nTRGIN TEST")
+    for tx, site in enumerate(uut.pg_sites):
+        sx = uut.modules[site]
+        if tx == 0:
+            print("trigger from local TRGIN")
+            sx.TRG = 'enable'
+            sx.TRG_DX = 'TRGIN'
+            sx.TRG_SENSE = 'rising'
+            sx.TRGOUT = 'PGIDX'
             
+            
+        if tx == 1:
+            print("run_site {} with cascade trigger from LH site".format(site))
+            sx.TRG = 'enable'
+            sx.TRG_DX = 'd{}'.format(site-1+1)
+            sx.TRG_SENSE = 'rising'
+            sx.TRGOUT = 'PGIDX'
+            
+            
+        if tx == 2:
+            print("chain site {} with cascade trigger from LH site".format(site))
+            sx.TRG = 'enable'
+            sx.TRG_DX = 'd{}'.format(site-1+1)
+            sx.TRG_SENSE = 'rising'
+            sx.TRGOUT = 'PGIDX'
+            
+            
+        if tx == 3:
+            print("chain site {} with cascade trigger from LH site".format(site))
+            sx.TRG = 'enable'
+            sx.TRG_DX = 'd{}'.format(site-1+1)
+            sx.TRG_SENSE = 'rising'
+            sx.TRGOUT = 'PGIDX'
+            
+        
+     """
+     
+     # todo now load a 4 x 1s sequence with PG4 trigger at the end of each second.
+# TIGA sings a round..
+# in the style of "partridge in a pear tree"
+
+STL sets up a series of 1s cycles, with d4 INDEX->TRG at fires at end of each second
+
+CYCLE,d3,d2,d1,d0      # flashes in second
+1 1,0,0,0
+2 1,2,0,0
+3 1,2,3,0
+4 1,2,3,4
+
+Load all sites.
+PG2 triggers on SOFT_TRIGGER
+PG3,4,5 trigger on SITE-1 TRG
+
+CYCLE PG2 PG3 PG4 PG5
+1 1,0,0,0 0,0,0,0 0,0,0,0 0,0,0,0
+2 1,2,0,0 1,0,0,0 0,0,0,0 0,0,0,0
+3 1,2,3,0 1,2,0,0 1,0,0,0 0,0,0,0
+4 1,2,3,4 1,2,3,0 1,2,0,0 1,0,0,0
+
+5 1,0,0,0 1,2,3,4 1,2,3,0 1,2,0,0
+6 1,2,0,0 1,0,0,0 1,2,3,4 1,2,3,0
+7 1,2,3,0 1,2,0,0 1,0,0,0 1,2,3,4
+8 1,2,3,4 1,2,3,0 1,2,0,0 1,0,0,0
+
+
+"""      
+                      
 
 def xmas_tree(args, uut):
     set_clkout(uut)
+    set_trgin(uut)
     
 def xmas_forest(args):
     for u in args.uuts:
