a7db977e46d47881cdb83090b73374fb6d255c47
Peter Milne <peter.milne@d-tacq.com>
2020-07-26 13:04:27 +0100

    test framework skeleton for multipe WRTT tests 	modified:   user_apps/TIGA/tiga-xmastree.py

--- user_apps/TIGA/tiga-xmastree.py
+++ user_apps/TIGA/tiga-xmastree.py
@@ -11,6 +11,7 @@
 import acq400_hapi
 import os
 import argparse
+import sys
 
 
 def _in_tolerance(y1, y2):
@@ -21,7 +22,36 @@ def in_tolerance(y1, y2):
         return _in_tolerance(y1, y2)
     else:
         return _in_tolerance(y2, y1)
-    
+   
+   
+class test_exec(object):
+
+    def __init__(self, f):
+        """
+        If there are no decorator arguments, the function
+        to be decorated is passed to the constructor.
+        """
+        #print("Inside __init__()")
+        self.f = f
+
+    def __call__(self, *args):
+        """
+        The __call__ method is not called until the
+        decorated function is called.
+        """
+        print("\n\nTEST: {}".format(self.f.__name__))
+        self.f(*args)
+        while True:
+            rc = input("continue Y/n")
+            if len(rc) == 0:
+                rc = 'Y'
+            if rc == 'Y':
+                break
+            if rc == 'N' or rc == 'n' or rc == 'q':
+                sys.exit(1)
+
+        
+@test_exec        
 def set_clkout(uut):
     print("\nCLKOUT TEST")
     clkout_freqs = {}
@@ -63,8 +93,8 @@ def set_clkout(uut):
         print("site:{} set:{:8.3e} actual:{:8.3e} {}".format(site, setpoint, actual, 
                             "PASS" if in_tolerance(setpoint, actual) else "FAIL"))
         
-        
-def set_trgin(uut): 
+@test_exec         
+def run_pg_cascade_trgin(uut): 
     print("\nTRGIN TEST")
     for tx, site in enumerate(uut.pg_sites):
         sx = uut.modules[site]
@@ -100,7 +130,7 @@ def set_trgin(uut):
             sx.TRGOUT = 'PGIDX'
             
         
-     """
+"""
      
      # todo now load a 4 x 1s sequence with PG4 trigger at the end of each second.
 # TIGA sings a round..
@@ -131,11 +161,41 @@ CYCLE PG2 PG3 PG4 PG5
 
 
 """      
-                      
+ 
+@test_exec   
+def run_local_wrtt(uut):
+    for tx, site in enumerate(uut.pg_sites):
+        sx = uut.modules[site]
+        if tx == 0:
+            print("trigger {} from local WRTT".format(site))
+            sx.TRG_DX = 'WRTT'
+            sx.WRTD_TX_MASK = 1<<(site+1)
+            sx.wrtd_commit_tx=1                 
+
+@test_exec 
+def run_local_wrtt_allsites(uut):
+    for tx, site in enumerate(uut.pg_sites):
+        sx = uut.modules[site]
+        sx.TRG_DX = 'WRTT'
+        if tx == 0:
+            sx.WRTD_TX_MASK = 0xfc
+            sx.wrtd_commit_tx=1        
+@test_exec             
+def run_global_wrtt_allsites(uut):
+    for tx, site in enumerate(uut.pg_sites):
+        sx = uut.modules[site]
+        sx.TRG_DX = 'd0'
+        if tx == 0:
+            sx.WRTD_TX_MASK = 1
+            sx.wrtd_commit_tx=1   
+    uut.s0.SIG_SRC_TRG_0 = 'WRTT0'     
+    
 
 def xmas_tree(args, uut):
-    set_clkout(uut)
-    set_trgin(uut)
+    for test in [ set_clkout, run_pg_cascade_trgin, run_local_wrtt, run_local_wrtt_allsites, run_global_wrtt_allsites]:
+        test(uut)
+    
+    
     
 def xmas_forest(args):
     for u in args.uuts:
