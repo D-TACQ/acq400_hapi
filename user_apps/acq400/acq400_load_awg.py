2a96790cc5257345210d21352046dcb9512c3ba3
dt100 <dt100@andros.d-tacq>
2020-09-04 18:27:43 +0100

    pgm add timing, repetition function  Committer: dt100 <dt100@andros.d-tacq> 	modified:   user_apps/acq400/acq400_load_awg.py

--- user_apps/acq400/acq400_load_awg.py
+++ user_apps/acq400/acq400_load_awg.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 """
 upload file to AWG for a one-shot play
@@ -23,6 +23,21 @@ from acq400_hapi import netclient as netclient
 import argparse
 
 
+from functools import wraps
+from time import time
+from time import sleep
+
+def timing(f):
+    @wraps(f)
+    def wrap(*args, **kw):
+        ts = time()
+        result = f(*args, **kw)
+        te = time()
+        print('TIMING:func:%r took: %2.2f sec' % (f.__name__, te-ts))
+        return result
+    return wrap
+
+
 def file_extender(fd, ext_count):
     buf = fd.read()
     buf0 = buf
@@ -34,13 +49,28 @@ def file_extender(fd, ext_count):
     return buf
         
 
-def load_awg(args):
-    uut = acq400_hapi.Acq400(args.uuts[0])
-    acq400_hapi.Acq400UI.exec_args_playtrg(uut, args)
-    with open(args.file, "rb") as fd:
-        uut.load_awg(file_extender(fd, args.awg_extend), autorearm=args.mode==2)
+@timing
+def load_awg(args, uut, rep):
+    if args.mode == 1 or (args.mode == 2 and rep == 0):
+        acq400_hapi.Acq400UI.exec_args_playtrg(uut, args)
+        with open(args.file, "rb") as fd:
+            uut.load_awg(file_extender(fd, args.awg_extend), autorearm=args.mode==2)
     if args.soft_trigger:
         uut.s0.soft_trigger = '1'
+
+@timing
+def wait_completion(args, uut):
+   while uut.modules[args.aosite].task_active == '1' or  uut.modules[args.aosite].completed_shot == '0':
+       sleep(0.1)
+       # print("polling completion")
+
+def load_awg_top(args):
+    uut = acq400_hapi.Acq400(args.uuts[0])
+    for rep in range(0, args.reps):
+        if rep > 0:
+            print("rep {}".format(rep))
+        load_awg(args, uut, rep)
+        wait_completion(args, uut)
 
 
 def run_main():
@@ -49,9 +79,12 @@ def run_main():
     parser.add_argument('--mode', default=2, type=int, help="mode: 1 oneshot, 2 oneshot_autorearm")
     parser.add_argument('--awg_extend', default=1, type=int, help='Number of times the AWG is repeated.')
     parser.add_argument('--soft_trigger', default=1, type=int, help='Emit soft trigger')
+    parser.add_argument('--reps', default=1, type=int, help='Repetitions')
+    parser.add_argument('--aosite', default=1, type=int, help='Site of AO module')
     acq400_hapi.Acq400UI.add_argument_playtrg(parser)
     parser.add_argument('uuts', nargs=1, help="uut ")
-    load_awg(parser.parse_args())
+
+    load_awg_top(parser.parse_args())
 
 # execution starts here
 
