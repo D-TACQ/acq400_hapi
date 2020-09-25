476606ed61be789f879ef449da564cdd5cc6066b
Peter Milne <peter.milne@d-tacq.com>
2020-09-25 11:05:45 +0100

    switch patterns on delay 	modified:   user_apps/acq400/acq400_continuous_burst_awg.py

--- user_apps/acq400/acq400_continuous_burst_awg.py
+++ user_apps/acq400/acq400_continuous_burst_awg.py
@@ -13,6 +13,7 @@ from a software point of view, it's continuous, but the waveform may run on a bu
 
 import acq400_hapi
 import argparse
+import time
 
 
 # AWG "Feature"
@@ -47,14 +48,18 @@ def run_awg(args):
         uut.modules[site].burst = '3,0,1'
         uut.modules[site].rtm_translen = args.length
         break
-        
-    work=acq400_hapi.awg_data.RainbowGen(uut, args.nchan, args.length, False)
+    
+    while True:    
+        work=acq400_hapi.awg_data.RainbowGen(uut, args.nchan, args.length, False)
 
-    for f in work.load(continuous=True):
-        print("Loaded %s" % (f))
-        input("hit return for next WF")
-        uut.modules[site].AWG_MODE_ABO = '1'
-        uut.modules[site].playloop_length = '0'
+        for f in work.load(continuous=True):
+            print("Loaded %s" % (f))
+            if args.delay:
+                time.sleep(args.delay)
+            else:
+                input("hit return for next WF")
+            uut.modules[site].AWG_MODE_ABO = '1'
+            uut.modules[site].playloop_length = '0'
 
     
     
@@ -63,6 +68,7 @@ def run_awg(args):
 def run_main():
     parser = argparse.ArgumentParser(description='acq1001 awg demo')
     parser.add_argument('--length', type=int, default=8192, help="AWG length")
+    parser.add_argument('--delay', type=int, default=0, help="auto switch on this delay")
     parser.add_argument('--trgDX', type=int, default=0, help="trigger DX line")
     parser.add_argument('uuts', nargs=1, help="uut ")
     run_awg(parser.parse_args())
