ea1ccc81c7955e060f2761d5890f2bd4ce366a79
Peter Milne <peter.milne@d-tacq.com>
2020-10-07 12:07:15 +0100

    [pgm@hoy5 utils]$ ./getmonitor.py --verbose=2 --key='VA[PN]' acq2106_119 acq2106_276 acq2106_119 VAN -13.1 acq2106_119 VAP 12.78 acq2106_276 VAN -13.0 acq2106_276 VAP 12.89
    	new file:   getmonitor.py
    	deleted:    getvolts.py

---
+++ user_apps/utils/getmonitor.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+'''
+Created on 6 Oct 2020
+
+@author: pgm
+
+Pulls one of the ACQ400 http ajax monitor files, parses and prints user selected values
+'''
+
+
+import urllib.request
+import argparse
+from lxml import etree
+import lxml
+import xml.etree.ElementTree as ElementTree
+import re
+
+def getvolts(uut, monxml, kex, verbose=0): 
+    while True:
+        try:   
+            content = urllib.request.urlopen("http://{}/d-tacq/data/{}".format(uut, monxml)).read()
+            root = etree.fromstring(content)
+            break
+        except lxml.etree.XMLSyntaxError:
+            continue
+        
+    for acqdata in root.xpath('//acqData'):
+        key = acqdata.attrib['n']
+        if kex.match(key):
+            value = acqdata[0].text
+            if verbose == 2:
+                print(uut, key, value)
+            elif verbose == 1:
+                print(key, value)
+            else:
+                print(value)
+
+
+def run_main():
+    parser = argparse.ArgumentParser(description='getvolts')
+    parser.add_argument('--verbose', default=1, type=int, help='0: print value, 1: print key,value, 2: print uut,key, value')
+    parser.add_argument('--monxml', default="volts.xml", help='monitor file to request and parse')
+    parser.add_argument('--key', default='VA.', help='key, could be regex')
+    parser.add_argument('uuts', nargs='+')
+    
+    
+    args = parser.parse_args()
+    kex = re.compile(args.key)
+    for uut in args.uuts:
+        getvolts(uut, args.monxml, kex, verbose=args.verbose)
+
+if __name__ == '__main__':
+    run_main()
+
+
