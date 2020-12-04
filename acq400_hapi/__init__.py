eb1cb52a570f2f34131a53e54c45b66a8e1251c1
dt100 <dt100@naboo.d-tacq>
2020-12-04 12:45:05 +0000

    Only import RADCELF if python version >= 3

--- acq400_hapi/__init__.py
+++ acq400_hapi/__init__.py
@@ -5,8 +5,10 @@ from .acq400 import Acq400, STATE, AcqPorts, ChannelClient, MgtDramPullClient, s
 from .acq400 import Acq2106
 from .acq400 import Acq2106_Mgtdram8
 from .acq400 import Acq2106_TIGA
-from .rad_dds import RAD3DDS
-from .rad_dds import AD9854
+import sys
+if sys.version_info > (3, 0):
+    from .rad_dds import RAD3DDS
+    from .rad_dds import AD9854
 from .shotcontrol import *
 from . import cleanup
 from . import awg_data
