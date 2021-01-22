4782eecfd07c64a6a47ba9fa04d04fcaf7916903
Sean <sean.alsop@d-tacq.co.uk>
2021-01-22 12:11:31 +0000

    Dropped python2 support for python3 & added better validation options.

--- test_apps/judgement_validation.py
+++ test_apps/judgement_validation.py
@@ -21,35 +21,47 @@ def get_args():
     parser.add_argument('--threshold', type=int, default=1000,
                         help="How close to ideal the judgement criteria are.")
 
+    parser.add_argument('--validation', type=int, default=1,
+                        help="validation type - 1: Use default 3 sine wave "
+                        "burst, 2: Use first buffer as validator +- threshold")
+
     parser.add_argument('uut', nargs=1, help="uut ")
     args = parser.parse_args()
     return args
 
 
-def check_channel(channel):
-    global upper
-    global lower
-    check_upper = upper - channel
-    check_lower = channel - lower
+def check_channel(channel, validation):
+    check_upper = validation[0] - channel
+    check_lower = channel - validation[1]
     if (check_upper < 0).any() or (check_lower < 0).any():
         return True
     return False
 
 
-def generate_boundary():
-    x = np.linspace(0, 6*np.pi, 750)
-    y = 3000 * np.sin(x)
-    y2 = np.concatenate((y, np.zeros(274)))
-    y3 = np.concatenate((y2, y2))
-    y3[0] = np.nan
-    y3[1024] = np.nan
-    return y3
+def generate_boundary(validation, nchan, bufferlen, threshold):
+    if validation == 1:
+        x = np.linspace(0, 6*np.pi, 750)
+        y = 3000 * np.sin(x)
+        y2 = np.concatenate((y, np.zeros(274)))
+        y3 = np.concatenate((y2, y2))
+        y3[0] = np.nan
+        y3[1024] = np.nan
+        data = [y3] * nchan
+        validation_data = [[ch + threshold, ch - threshold] for ch in data]
+        return validation_data
+
+    elif validation == 2:
+        raw_data = sys.stdin.read(bufferlen)
+        raw_data = np.fromstring(raw_data, dtype=np.int16)
+        data = raw_data.reshape((-1, nchan)).T
+        validation_data = [[ch + threshold, ch - threshold] for ch in data]
+        return validation_data
 
 
-def generate_fail_report(data):
+def generate_fail_report(data, validation_data):
     fail_report = []
     with concurrent.futures.ThreadPoolExecutor() as executor:
-        results = executor.map(check_channel, data)
+        results = executor.map(check_channel, data, validation_data)
         for result in results:
             fail_report.append(result)
     return fail_report
@@ -65,7 +77,6 @@ def make_data_dir(directory, verbose):
 
 
 def save_data(data, file_name):
-    # print("Saving judgement failed buffer to file: {}".format(file_name))
     data.tofile(file_name)
     return None
 
@@ -77,18 +88,15 @@ def main():
     file_name = "./{}/{}"
     make_data_dir(uut, 0)
 
-    global upper
-    global lower
-
     buffer_num = 0
     uut_object = acq400_hapi.Acq400(uut)
     nchan = uut_object.nchan()
     bufferlen = int(uut_object.s0.bufferlen)
 
-    y = generate_boundary()
-
-    upper = y + args.threshold
-    lower = y - args.threshold
+    validation_data = generate_boundary(args.validation,
+                                        nchan,
+                                        bufferlen,
+                                        args.threshold)
 
     while True:
 
@@ -96,11 +104,11 @@ def main():
         buffer_num += 1
         file_name = "./{}/{:05d}".format(uut, buffer_num)
 
-        raw_data = sys.stdin.read(bufferlen)
+        raw_data = sys.stdin.buffer.read(bufferlen)
         raw_data = np.fromstring(raw_data, dtype=np.int16)
         data = raw_data.reshape((-1, nchan)).T
 
-        fail_list = np.array(generate_fail_report(data))
+        fail_list = np.array(generate_fail_report(data, validation_data))
         pv_check = np.array(epics.caget('{}:JDG:CHX:FAIL:ALL'.format(uut)))[1:]
 
         if fail_list.any() or pv_check.any():
