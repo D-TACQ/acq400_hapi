311566eb2afb1099a991b458d7aa57cf7ca87b5d
Sean <sean.alsop@d-tacq.co.uk>
2021-04-08 11:34:07 +0100

    Moved plotting to fail condition block. 	modified:   test_apps/timbus_val.py

--- test_apps/timbus_val.py
+++ test_apps/timbus_val.py
@@ -24,16 +24,19 @@ def compare_bursts(first_burst, burst_n):
         burst_n = burst_n[0:len(first_burst)]
 
     if np.allclose(burst_n, first_burst, rtol=0, atol=15000000):
-        plt.plot(first_burst)
-        plt.plot(burst_n)
-        plt.plot(np.abs(first_burst - burst_n))
-        plt.show()
-        # print(first_burst)
-        # print(burst_n)
+        # plt.plot(first_burst)
+        # plt.plot(burst_n)
+        # plt.plot(np.abs(first_burst - burst_n))
+        # plt.show()
         return True
     else:
         print(first_burst)
         print(burst_n)
+        plt.plot(first_burst)
+        plt.plot(burst_n)
+        plt.plot(np.abs(first_burst - burst_n))
+        plt.show()
+
         # plt.plot(first_burst[0::10])
         # plt.plot(burst_n[0::10])
         return False
@@ -41,19 +44,17 @@ def compare_bursts(first_burst, burst_n):
 
 def main():
     args = get_args()
-    # uut = args.uut[0]
     NCHAN = 10
-
     data = np.fromfile(args.file, dtype=np.int32)
 
     # reshape
     data = data.reshape((-1, NCHAN))
     CH01 = data[0:,0]
     timbus = data[0:,8]
-    plt.plot(CH01)
-    plt.plot(timbus)
-    plt.grid(1)
-    plt.show()
+    # plt.plot(CH01)
+    # plt.plot(timbus)
+    # plt.grid(1)
+    # plt.show()
     #test = np.where(data[8] ,a, 1)
 
     mask = np.argwhere(np.bitwise_and(timbus, 0x10000000))
@@ -80,23 +81,17 @@ def main():
             if init[0] == 0:
                 first_burst.append(row[0])
                 init[1] = 1
-                # print("DEBUG1")
             else:
                 burst_n.append(row[0])
                 init = [2, 1]
-                # print("DEBUG2")
 
             continue
         else:
-            # pri
 
             if init[1] == 1:
                 if init[0] == 0:
                     init[0] = 1
-                # print("DEBUG3")
             if init == [2, 1]:
-                # print("DEBUG4")
-                # sys.exit(1)
                 if not compare_bursts(first_burst, burst_n):
                     print("Problem detected in burst. Quitting now.")
                     sys.exit(1)
