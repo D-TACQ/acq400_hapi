e83328920328b9ec5cb02f95415dd2fd0220f487
Peter Milne <peter.milne@d-tacq.com>
2021-06-08 22:31:06 +0100

    .. generate a set of waveforms, and store them as raw ./user_apps/utils/make_waves.py --offset_per_channel=0.1 generate scale_raw save_raw plot1 .. load waveforms as raw and plot ./user_apps/utils/make_waves.py load_raw plot1
    	modified:   user_apps/utils/make_waves.py

--- user_apps/utils/make_waves.py
+++ user_apps/utils/make_waves.py
@@ -9,12 +9,79 @@ Created on 8 Jun 2021
 import numpy as np
 import argparse
 from asyncio.protocols import DatagramProtocol
+import sys
+
+import matplotlib.pyplot as plt
 
 
 def ramp(args):    
     return np.linspace(-args.amp, args.amp, num=args.len)
 
-OPS = ( "generate", "store_files", "load_files", "load_uut", "UUT", "continuous", "autorearm" )
+
+
+def np_type(args):   
+    np.int32 if args.res != 16 else np.int16
+    
+def make_waves(args, iarg):
+    print("make_waves")
+    ch0 = ramp(args)
+    print("shape ch0 {}".format(ch0.shape))
+    args.chx = np.zeros([args.len, args.nchan])
+    for ch in range(0, args.nchan):
+        offset = (ch - args.nchan/2)*args.offset_per_channel
+        args.chx[:,ch] = ch0 + offset
+        
+        
+def store_files(args, iarg):
+    print("store_files")
+    if args.root:
+        for ch in range(0, args.nchan):
+            offset = (ch - args.nchan/2)*args.offset_per_channel
+            args.chx[:,ch].tofile("{}/ch{:02d}_{}_{}+{:.2}V.dat".
+                             format(args.root, ch, args.len, args.amp, offset))
+
+def scale_raw(args, iarg):
+    scale = (1 << (args.res-1))/args.vmax
+    args.raw = (args.chx * scale).astype(np_type(args))
+    
+    
+def store_raw(args, iarg):    
+    print("store_raw")
+    args.raw.tofile("{}/myfile.raw".format(args.root))
+
+def load_files(args, iarg):
+    print("load_files @@todo")
+        
+def load_raw(args, iarg):
+    print("load_raw")
+    args.raw = np.fromfile("{}/myfile.raw".format(args.root), np_type(args)).reshape((-1,args.nchan))
+
+def plot1(args, iarg):
+    print("shape of plot data {}".format(args.raw.shape))
+    plt.plot(args.raw)
+    plt.show()
+    
+class ExitException(BaseException):
+    pass
+
+
+
+
+def load_uut(args, iarg):
+    print("load_uut {}".format(" ".join(args.ops[iarg:])))
+    print("exit")
+    raise ExitException
+
+
+def is_param(args, iarg):
+    print("is_param")
+    
+OPS = { "generate": make_waves, 
+       "store_files": store_files, "load_files": load_files, 
+       "store_raw": store_raw, "load_raw": load_raw,
+       "scale_raw": scale_raw,
+       "plot1": plot1,
+       "load_uut": load_uut, "UUT": is_param, "continuous": is_param, "autorearm": is_param }
         
 def ui():
     parser = argparse.ArgumentParser(description='make_awg_data')
@@ -24,6 +91,7 @@ def ui():
     parser.add_argument('--ncycles', default=8,     type=int,   help="number of waveform cycles in set")
     parser.add_argument('--offset_per_channel', default=0.0, type=float,     help="offset in volts *ch")
     parser.add_argument('--res',   default=16, type=int,        help="word size in bits")
+    parser.add_argument('--vmax',   default=10.0, type=float,   help="full scale voltage (always symmetrical twos comp")
     parser.add_argument('--fxn', default="ramp",                help="function to execute")
     parser.add_argument('--root', default='DATA',               help="offset in volts *ch")
     parser.add_argument('--merge', default=1,                   help="merge data into single binary")
@@ -35,33 +103,18 @@ def ui():
         args.fn = eval('ramp')
     else:
         print("ERROR: fxn {} not available".format(args.fxn))
-    return args
-
-    
-
-def make_waves(args):
-    ch0 = ramp(args)
-    chx = np.zeros([args.len, args.nchan])
-    for ch in range(0, args.nchan):
-        offset = (ch - args.nchan/2)*args.offset_per_channel
-        chx[:,ch] = ch0 + offset
-        if args.root:
-            chx[:,ch].tofile("{}/ch{:02d}_{}_{}+{}V.dat".
-                             format(args.root, ch, args.len, args.amp, offset))
-        
-def store_files(args):
-    pass
-    
-        
-    
+    return args    
 
 def run_main():
     args = ui()
-    for op in args.ops:
-        if op == "generate":
-            make_waves(args)
-        elif op == "store_files":
-            store_files(args)
+    for iarg, op in enumerate(args.ops):
+        try:
+            OPS[op](args, iarg)
+        except ExitException:
+            print("quitting time")
+            sys.exit(0)
+        except Exception as e:
+            print("rejected {} {}".format(op, e))
         
 
 # execution starts here
