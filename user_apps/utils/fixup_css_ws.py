686275872351bc51e7c9053fc876fb623fa7e715
Peter Milne <peter.milne@d-tacq.com>
2021-06-21 09:20:44 +0100

    converts BS to FS where needed. Actually, this is a dead end. IF we create the WS on WIN, then all the slashes are the right type and we can just sub the name  On branch master 	modified:   user_apps/utils/fixup_css_ws.py

--- user_apps/utils/fixup_css_ws.py
+++ user_apps/utils/fixup_css_ws.py
@@ -15,8 +15,29 @@ def ui():
     parser.add_argument('--user', default=None, help="user name to change")
     parser.add_argument('--uuts', default=None, help="uuts to change [uut[1][,uut2[,uut3...]]]]")
     parser.add_argument('ws', nargs='+', help="workspace[s] to change")
-    return parser.parse_args()
+    args = parser.parse_args()
+    if args.user and args.user.find('\\') != -1:
+        args.user_windows = True
+    else:
+        args.user_windows = False
+    return args
    
+
+# ASCII ROOLS OK, BackSlash, ForwardSlash
+BS = 0x5c
+FS = 0x2f
+
+# URI is in Unix notation, AND must have leading FS
+'''
+[pgm@hoy5 acq400_hapi]$ hexdump -C /home/pgm/SANDBOX/DOC-215920-01-FAT/.metadata/.plugins/org.eclipse.core.resources/.projects/ACQ400/.location
+00000000  40 b1 8b 81 23 bc 00 14  1a 25 96 e7 a3 93 be 1e  |@...#....%......|
+00000010  00 2d 55 52 49 2f 2f 66  69 6c 65 3a 2f 43 3a 2f  |.-URI//file:/C:/|
+00000020  55 73 65 72 73 2f 70 67  6d 30 30 2f 50 52 4f 4a  |Users/pgm00/PROJ|
+00000030  45 43 54 53 2f 41 43 51  34 30 30 43 53 53 2f 41  |ECTS/ACQ400CSS/A|
+00000040  43 51 34 30 30 00 00 00  00 00 00 00 00 c0 58 fb  |CQ400.........X.|
+00000050  f3 23 bc 00 14 1a 51 f3  8c 7b bb 77 c6           |.#....Q..{.w.|
+
+'''
 
 def fixup(args, ws):
     print("fixup {}".format(ws))
@@ -37,8 +58,30 @@ def fixup(args, ws):
                     print("MATCH {} start:{} end:{}".format(match, match.start(), match.end()))
                     file_changed = True
                     wmode = 'w'
-                text = text.replace('/home/pgm', args.user)
-                
+                    
+                if file_changed:
+                    text = text.replace('/home/pgm', args.user)
+
+                    if args.user_windows:
+                        tl = list(text)
+                        cursor = 0
+                        while True:
+                            cursor = text[cursor:].find(args.user)
+                            if cursor == -1:
+                                break
+                            for ii, cc in enumerate(tl[cursor:]):
+#                                print("ii {} cc {}".format(ii, cc))
+                                if tl[cursor+ii] == '"' and ii > 0:
+                                    break
+                                if tl[cursor+ii] == '/':
+                                    #print("replacing ..")
+                                    tl[cursor+ii] = '\\'
+                            
+                            cursor += ii
+                            print("set cursor {}".format(cursor))
+                            
+                    text = ''.join(tl)
+
                 
             except UnicodeDecodeError:
                 with open(path, 'rb') as fp:
@@ -56,7 +99,28 @@ def fixup(args, ws):
                     while text[headroom_end] == 0:
                         headroom_end += 1
                     print("trailer {}, headroom {}".format(text[trailer_start:trailer_end], headroom_end - trailer_end))
+
+                    # binary URI must be pure Unix with single '/'
                     sub_bytes = args.user.encode()
+                    sub2 = bytearray(len(sub_bytes)+1)
+                    dst = 0
+                    sub2[dst] = FS   # file:/
+                    dst += 1
+                    for src in range(0, len(sub_bytes)):
+#                        print("{:02x}".format(sub_bytes[src]), end="")
+                        if sub_bytes[src] != BS:
+                            sub2[dst] = sub_bytes[src]
+                            dst += 1
+                        else:                            
+                            if dst > 0 and sub2[dst-1] == FS:
+                                continue
+                            else:
+                                sub2[dst] = FS
+                                dst += 1
+#                    print("")
+                    print("before: {}".format(sub_bytes))                    
+                    sub_bytes = sub2[0:dst]
+                    print("after: {}  ... cleaned for URI".format(sub_bytes))
                     if len(sub_bytes) > (match.end() - match.start()) + (headroom_end - trailer_end):
                         print("ERROR: unable to fit new name {} in binary file".format(args.user))
                         sys.exit(1)
