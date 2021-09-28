cdd7b0b6d5d8a06006b91abd0406b86175e5694d
Peter Milne <peter.milne@d-tacq.com>
2021-09-28 15:59:39 +0100

    autpep8

--- user_apps/acq400/acq400_remote_eval.py
+++ user_apps/acq400/acq400_remote_eval.py
@@ -28,15 +28,15 @@ def handle_line(uuts, line):
         return
     for uut in uuts:
         uut.s0.sr(line)
-        
+
 def remote_script(args):
     print("WORKTODO DO NOT USE")
     uuts = [acq400_hapi.Acq400(u) for u in args.uuts]
-    
+
     if args.trace:
         for uut in uuts:
             uut.s0.trace = args.trace
-    
+
     if args.script == '-':
         for line in sys.stdin:            
             handle_line(uuts, line)
@@ -44,21 +44,21 @@ def remote_script(args):
         with open(args.script) as f:
             for line in f:                
                 handle_line(uuts, line)
-                
+
 def remote_eval(args):
     uuts = [acq400_hapi.Acq400(u) for u in args.uuts]
-       
+
     for u in uuts:
-	sx, kvp = args.eval.split('.')
-	kvp2 = kvp.split('=')
+        sx, kvp = args.eval.split('.')
+        kvp2 = kvp.split('=')
         key = kvp2[0]
         if len(kvp2) > 1:
             value = kvp2[1]
-	    cmd = 'u.svc[sx].set_knob(key, value)'
-	else:
+            cmd = 'u.svc[sx].set_knob(key, value)'
+        else:
             cmd = 'u.svc[sx].get_knob(key)'
-	rx = eval(cmd)
-	print("{} {} => {}".format(u.uut, cmd, rx))
+        rx = eval(cmd)
+        print("{} {} => {}".format(u.uut, cmd, rx))
 
 def run_main():
     parser = argparse.ArgumentParser(description='acq400_remote_script')
