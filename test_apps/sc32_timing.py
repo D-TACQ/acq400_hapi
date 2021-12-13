e1746cc4b5a8bed0bcd3d0d1f3b8673e04f87b38
Peter Milne <peter.milne@d-tacq.com>
2021-12-13 17:23:58 +0000

    set all  ${uut}:${site}:G1_cc first, using HAPI. Result on D-TACQVPN for 96 Set, 96 Get: [pgm@hoy5 acq400_hapi]$ SITECLIENT_TRACE=1 python ./test_apps/sc32_timing.py acq2106_132 Siteclient(acq2106_132, 4220) >MODEL ... Siteclient(acq2106_132, 4221) >SC32:G1:04=0 Siteclient(acq2106_132, 4221) < Siteclient(acq2106_132, 4221) >SC32:G1:05=1 Siteclient(acq2106_132, 4221) < Siteclient(acq2106_132, 4221) >SC32:G1:06=2 Siteclient(acq2106_132, 4221) < Siteclient(acq2106_132, 4221) >SC32:G1:07=3 Siteclient(acq2106_132, 4221) <
    TIMING:func:'set_all_gains' took: 17.08 sec
    get_all_gains (1, 3, 5)
    
    Siteclient(acq2106_132, 4225) >SC32:G1:31
    Siteclient(acq2106_132, 4225) <SC32:G1:31 1000
    Siteclient(acq2106_132, 4225) >SC32:G1:32
    Siteclient(acq2106_132, 4225) <SC32:G1:32 1
    1,100,1000,1,1,100,1000,1,1,100,1000,1,1,100,1000,1,1,100,1000,1,1,100,1000,1,1,100,1000,1,1,100,1000,1,1,100,1000,1,1,100,1000,1,1,100,1000,1,1,100,1000,1,1,100,1000,1,1,100,1000,1,1,100,1000,1,1,100,1000,1,1,100,1000,1,1,100,1000,1,1,100,1000,1,1,100,1000,1,1,100,1000,1,1,100,1000,1,1,100,1000,1,1,100,1000,1
    TIMING:func:'get_all_gains' took: 16.86 sec
    TIMING:func:'run_test' took: 33.81 sec
    
     On branch master
    	new file:   test_apps/sc32_timing.py

---
+++ test_apps/sc32_timing.py
@@ -0,0 +1,56 @@
+'''
+Created on 13 Dec 2021
+
+@author: pgm
+'''
+
+import acq400_hapi
+import argparse
+
+from acq400_hapi import timing as timing
+
+
+def get_args():
+    parser = argparse.ArgumentParser(description = "setting SC32 gains .. timing")
+    parser.add_argument('--sites', default='(1,3,5)', help="site [1]")
+    parser.add_argument('uut', nargs='+', help="uut")
+    args = parser.parse_args()
+    args.sites = eval(args.sites)
+    return args
+
+
+@timing
+def set_all_gains(args, uut):
+    print("set_all_gains {}".format(args.sites))
+    for site in args.sites:
+        for ch in range(1,33):
+            value = ch%4
+            uut.svc["s{}".format(site)].set_knob("SC32_G12_{:02d}".format(ch), value)
+
+@timing
+def get_all_gains(args, uut):
+    value = ""
+    print("get_all_gains {}".format(args.sites))
+    for site in args.sites:
+        for ch in range(1,33):
+            if len(value):
+                value += ","
+            value += uut.svc["s{}".format(site)].get_knob("SC32_G12_{:02d}".format(ch))
+
+    print(value)
+
+@timing
+def run_test(args, uut):
+    set_all_gains(args, uut)
+    get_all_gains(args, uut)
+    
+def main():
+    args = get_args()
+    
+    uut = acq400_hapi.factory(args.uut[0])
+    run_test(args, uut)
+
+
+
+if __name__ == '__main__':
+    main()
