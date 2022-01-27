0e8600de78b414da621f2bd1ed83fcd7d42e6621
Peter Milne <peter.milne@d-tacq.com>
2022-01-27 10:51:02 +0000

    second plot is a framescan example  On branch master 	modified:   user_apps/analysis/fs_delay_gen.py

--- user_apps/analysis/fs_delay_gen.py
+++ user_apps/analysis/fs_delay_gen.py
@@ -52,12 +52,34 @@ def calc_lpd(args):
     plt.yticks(np.arange(0, 2, step=1))
     
     ax2 = plt.subplot(nsp, 1, 3, sharex=ax0)
-    ax2.set_ylabel("{}".format("DAC3 LPA"))
+    ax2.set_ylabel("{}".format("AO3 LPA"))
     plt.step(ctimes, lpa)
     
     ax2.set_xlabel("Nano Seconds")
     plt.show()
-    return len(states)/2    # return dwell in us
+    return len(states)//2    # return dwell in us
+
+def calc_frame(args, dwell):
+    ltime = args.line*dwell
+    ltimes = [ t for t in range(0, ltime)]    
+    ldac = [ x//dwell * 5 / args.line for x in ltimes ]
+    
+    ftimes = [ t for t in range(0, ltime*args.frame) ]
+    fdac = [ x//ltime * 5 * args.frame for x in ftimes ]
+    
+
+    
+    ax0 = plt.subplot(2, 1, 1)
+    ax0.set_title("Raster Scan Example")
+    ax0.set_ylabel("AO1 Frame V") 
+    ax0.set_xlabel("Micro Seconds")      
+    plt.step(ftimes, fdac)
+    
+    ax1 = plt.subplot(2, 1, 2)
+    ax1.set_ylabel("AO2 Line V")   
+    ax1.set_xlabel("Micro Seconds")
+    plt.step(ltimes, ldac)
+    plt.show()
 
 def run_main():
     parser = argparse.ArgumentParser(description='multiplot')
@@ -65,11 +87,14 @@ def run_main():
     parser.add_argument('--d2', type=int, default=25, help="last_delay+1 (ticks)")
     parser.add_argument('--ds', type=int, default=1, help="delay step (ticks)")
     parser.add_argument('--tpi', type=int, default=30, help="ticks per interval")
-    parser.add_argument('--tns', type=float, default=16.6666, help="tick nsec")    
+    parser.add_argument('--tns', type=float, default=16.6666, help="tick nsec")
+    parser.add_argument('--frame', type=int, default=100, help="frame line count")
+    parser.add_argument('--line', type=int, default=100, help="line pixel count")    
     
     
     args = parser.parse_args()
     dwell = calc_lpd(args)
+    calc_frame(args, dwell)
 
 if __name__ == '__main__':
     run_main()
