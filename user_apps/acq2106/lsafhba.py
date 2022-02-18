aea0ccbc566cea69675c5e45c5d7fc93f20f51c2
Peter Milne <peter.milne@d-tacq.com>
2022-02-18 19:44:39 +0000

    generates correct config json VI, SP32, VO from introspection of UUT  On branch master 	modified:   user_apps/acq2106/lsafhba.py

--- user_apps/acq2106/lsafhba.py
+++ user_apps/acq2106/lsafhba.py
@@ -23,10 +23,54 @@ def mtype(mod):
     return "{}{}".format(mt, 32 if mod.data32=='1' else 16)
 
 def save_VI(cfg, indent, uut):
-    cfg.write('{}"{}": {}\n'.format(indent, "AI32", 66))
+    NC = { 'AI16': 0, 'AI32': 0, 'DI32': 0, 'SP32': 0 }
+    for s in [int(s) for s in uut.sites]:
+        mod = uut.modules[s]
+        model = mod.MODEL.split(" ")[0]
+        d32 = mod.data32 == '1'
+        nchan = int(mod.NCHAN)
+        is_adc = mod.is_adc.split(" ")[0] == '1'
+        mt = mod.MTYPE
+        
+        
+        if is_adc:
+            NC["AI{}".format(32 if d32 else 16)] += nchan
+        elif mt[0] == '6':
+            NC['DI32'] += 1 
+    
+    len_vi = 0    
+    for key, value in NC.items():
+        if value > 0:
+            len_vi += value * (2 if key == "AI16" else 4)
+            cfg.write('{}"{}": {}\n'.format(indent, key, value))
+            
+    sp32 = (16*4 - len_vi%64) // 4
+    cfg.write('{}"{}": {}\n'.format(indent, "SP32", sp32))
     
 def save_VO(cfg, indent, uut):
-    cfg.write('{}"{}": {}\n'.format(indent, "AO32", 66))
+    NC = { 'AO16': 0, 'AO32': 0, 'DO32': 0 }
+    
+    for s in [int(s) for s in uut.sites]:
+        mod = uut.modules[s]
+        model = mod.MODEL.split(" ")[0]
+        d32 = mod.data32 == '1'
+        nchan = int(mod.NCHAN)
+        is_adc = mod.is_adc.split(" ")[0] == '1'
+        mt = mod.MTYPE
+        
+        if is_adc:
+            pass
+        elif mt[0] == '4':
+            NC["AO{}".format(32 if d32 else 16)] += nchan
+        elif mt[0] == '6':
+            NC['DO32'] += 1 
+    
+    len_vo = 0    
+    for key, value in NC.items():
+        if value > 0:
+            len_vo += value * (2 if key == "AO16" else 4)
+            cfg.write('{}"{}": {}\n'.format(indent, key, value))
+    
     
 def save_config_skel(args, cfile, conns, uuts):
     indent = ''
@@ -43,8 +87,8 @@ def save_config_skel(args, cfile, conns, uuts):
             indent += '\t'          
             cfg.write('{}"DEVNUM": {},\n'.format(indent, value.dev))
             cfg.write('{}"name": "{}"\n'.format(indent, value.uut))
-            cfg.write('{}"type": "pcs"\n'.format(indent))
-            cfg.write('{}"sync_role": "{}\n'.format(indent, "master" if ii==0 else "slave"))
+            cfg.write('{}"type": "pcs",\n'.format(indent))
+            cfg.write('{}"sync_role": "{}",\n'.format(indent, "master" if ii==0 else "slave"))
             
             cfg.write('{}"VI": {}\n'.format(indent, '{'))
             indent += '\t'
