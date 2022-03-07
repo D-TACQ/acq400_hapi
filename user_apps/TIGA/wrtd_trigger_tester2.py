81daa391ab4cbf653b531a2ef04d728b6acc5626
Peter Milne <peter.milne@d-tacq.com>
2022-03-07 11:16:47 +0000

    simple trigger tester .. does it ever miss?.  On branch master 	new file:   user_apps/TIGA/wrtd_trigger_tester2.py

---
+++ user_apps/TIGA/wrtd_trigger_tester2.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+'''
+wrtd_trigger_tester : exercises wrtd_txa on WRTT0
+Created on 17 Aug 2021
+
+@author: pgm
+
+usage: pg_trigger_txq_tester.py : how close can we reliably separate two txq messages?
+
+'''
+import acq400_hapi
+import os
+import argparse
+import sys
+import time
+
+    
+def get_args():
+    parser = argparse.ArgumentParser(description='wrtd_trigger_tester')    
+    parser.add_argument('--shots', default=10, type=int, help="number of shots") 
+    parser.add_argument('uut', nargs='+', help="uuts")    
+    return parser.parse_args()
+
+   
+#acq2106_345:0:SIG:TRG_S2:RESET
+    
+def trigger_test_init(args, uuts):
+    for u in uuts:
+        u.cC.WR_WRTT0_RESET = '1'
+        u.cC.WR_WRTT1_RESET = '1'
+        u.s0.SIG_SRC_TRG_0 = 'WRTT0'    # default on WR system
+        u.s0.SIG_SRC_TRG_1 = 'WRTT1'    # repalce STRG.
+    
+ 
+
+        
+        
+def trigger_test(args):
+    uuts = [ acq400_hapi.factory(u) for u in args.uut ]
+    
+    trigger_test_init(args, uuts)
+    
+    count0 = [ acq400_hapi.Acq400.intpv(u.cC.WR_WRTT0_COUNT) for u in uuts ]
+
+    for shot in range(0, args.shots):
+        uuts[0].cC.wrtd_txa = '--at +1 1'
+        time.sleep(4)                   # let the counters ketchup
+        
+        count1 = [ acq400_hapi.Acq400.intpv(u.cC.WR_WRTT0_COUNT) for u in uuts ]
+        print(count1)
+          
+
+def main():
+    trigger_test(get_args())
+
+if __name__ == '__main__':
+    main()
