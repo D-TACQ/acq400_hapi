6b16647dc8d7e6b7e5f397afd4000365a9b397d0
Peter Milne <peter.milne@d-tacq.com>
2023-01-09 21:48:30 +0000

    include stim. 	modified:   user_apps/special/run_AquadB_movement.py

--- user_apps/special/run_AquadB_movement.py
+++ user_apps/special/run_AquadB_movement.py
@@ -16,9 +16,10 @@ OR, it could be a real movement. We don't care.
 import acq400_hapi
 import argparse
 import time
-
 import os
 import sys
+import numpy as np
+
 sys.path.append(os.path.dirname(os.path.realpath('../../user_apps')))
 
 from  user_apps.acq400 import acq400_stream_multi
@@ -26,15 +27,21 @@ from  user_apps.acq400 import acq400_stream_multi
 from enum import Enum
 
 class AquadB_callback:
-    State = Enum('State', ["WaitCountActive", "WaitCountStop", "Finished"])
-    def __init__(self, uut):
-        self.state = AquadB_callback.State.WaitCountActive
+    State = Enum('State', ["Init", "WaitCountActive", "WaitCountStop", "Finished"])
+    def __init__(self, args, uut):
+        self.state = AquadB_callback.State.Init
+        self.args = args
         self.uut = uut
         self.count = uut.s1.QEN_COUNT
         self.count_unchanged = 0
         pass
 
     def __call__(self):
+        if self.state == AquadB_callback.State.Init:
+            print("Pressing the trigger..")
+            self.args.uut_stim.s0.soft_trigger = 1
+            self.state = AquadB_callback.State.WaitCountActive
+
         newcount = self.uut.s1.QEN_COUNT
         if newcount != self.count:
             self.count_unchanged += 1
@@ -50,14 +57,32 @@ class AquadB_callback:
         self.count = newcount
         return self.state == AquadB_callback.State.Finished
 
+def load_stim(uut, dwg):
+    uut.load_awg(np.fromfile(dwg, dtype=np.uint32))
+    pc = 0
+    while acq400_hapi.intpv(uut.s1.AWG_ACTIVE) != 1:
+        if pc > 1:
+            print("polling for AWG_ACTIVE")
+        time.sleep(1)
+        pc += 1
 
 def main(args):
     uut = acq400_hapi.factory(args.uuts[0])
-    args.callback = AquadB_callback(uut)
+
+    if args.stim is not None:
+        if args.dwg is None:
+            print("ERROR: no dwg to load")
+            return
+        args.uut_stim = acq400_hapi.factory(args.stim)
+        load_stim(args.uut_stim, args.dwg)
+
+    args.callback = AquadB_callback(args, uut)
     acq400_stream_multi.run_stream(args)
 
 def get_parser():
     parser = acq400_stream_multi.get_parser()
+    parser.add_argument('--stim', default=None, help='stimulator uut')
+    parser.add_argument('--dwg', default=None, help='stimulator dwg file to load')
     return parser
 
 if __name__ == '__main__':
