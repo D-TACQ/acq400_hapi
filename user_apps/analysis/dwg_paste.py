e64189f1b4da5bca513329502899634512c4918d
peter milne <peter.milne@naboo>
2023-01-10 13:21:07 +0000

    dwg_paste.py .. pastes two columns of uint32 	new file:   user_apps/analysis/dwg_paste.py

---
+++ user_apps/analysis/dwg_paste.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+'''
+paste two binary files assume single column uint32
+eg to make a 2xDIO422 wave pattern
+'''
+
+
+import numpy as np
+import sys
+
+
+SRC = sys.argv[1]
+DST = "{}.2x32".format(SRC)
+
+raw1 = np.fromfile(SRC, np.uint32)
+raw2 = np.dstack((raw1,raw1))
+
+print(raw1.shape,raw2.shape)
+
+with open(DST, 'wb') as fp:
+    raw2.tofile(fp)
+
+
