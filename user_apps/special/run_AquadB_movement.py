ade19c60d2d10ed5f92c634bfd4d69b6442aba6b
Peter Milne <peter.milne@d-tacq.com>
2023-01-10 11:52:27 +0000

    finally a runner. buffer rate is higher than epics update rate  On branch master 	modified:   user_apps/special/run_AquadB_movement.py

--- user_apps/special/run_AquadB_movement.py
+++ user_apps/special/run_AquadB_movement.py
@@ -19,6 +19,7 @@ import time
 import os
 import sys
 import numpy as np
+import epics
 
 sys.path.append(os.path.dirname(os.path.realpath('../../user_apps')))
 
@@ -28,29 +29,42 @@ from enum import Enum
 
 class AquadB_callback:
     State = Enum('State', ["WaitCountActive", "WaitCountStop", "Finished"])
-    def __init__(self, args, uut):
+    
+    aqb_count = 0
+
+    def onChanges(pvname=None, value=None, char_value=None, **kw):
+        print('PV Changed! ', pvname, value, char_value, time.ctime())
+        AquadB_callback.aqb_count = value        
+
+    def __init__(self, args):
         self.state = AquadB_callback.State.WaitCountActive
+        self.monitor = epics.PV("{}:1:QEN:COUNT".format(args.uuts[0]), auto_monitor=True)
+        self.monitor.add_callback(AquadB_callback.onChanges)
         self.args = args
-        self.uut = uut
-        self.count = uut.s1.QEN_COUNT
         self.count_unchanged = 0
+        self.count = 0
         pass
 
-    def __call__(self):        
-        newcount = self.uut.s1.QEN_COUNT
+    BUFFERS_PER_EPICS_CB = 20
+    COUNT_DEADBAND = 100
+    def __call__(self):
+        newcount = AquadB_callback.aqb_count
         if newcount == self.count:
-            self.count_unchanged += 1                        
+            self.count_unchanged += 1
         else:
             self.count_unchanged = 0
-            
+        self.count = newcount
+
         if self.state == AquadB_callback.State.WaitCountActive and self.count_unchanged == 0:
             self.state = AquadB_callback.State.WaitCountStop
-        elif self.state == AquadB_callback.State.WaitCountStop and self.count_unchanged > 2:
-            self.state = AquadB_callback.State.Finished
+        
+        elif self.state == AquadB_callback.State.WaitCountStop:
+            if self.count > AquadB_callback.COUNT_DEADBAND and self.count_unchanged > AquadB_callback.BUFFERS_PER_EPICS_CB:
+                self.state = AquadB_callback.State.Finished
 
 
         print("AquadB_callback {} {}".format(self.state.name, newcount))
-        self.count = newcount
+        
         return self.state == AquadB_callback.State.Finished
 
 def load_stim(uut, dwg):
@@ -64,8 +78,6 @@ def load_stim(uut, dwg):
     uut.s1.dio422_TxEN = 1
 
 def main(args):
-    uut = acq400_hapi.factory(args.uuts[0])
-
     if args.stim is not None:
         if args.dwg is None:
             print("ERROR: no dwg to load")
@@ -73,7 +85,7 @@ def main(args):
         args.uut_stim = acq400_hapi.factory(args.stim)
         load_stim(args.uut_stim, args.dwg)
 
-    args.callback = AquadB_callback(args, uut)
+    args.callback = AquadB_callback(args)
     acq400_stream_multi.run_stream(args)
 #    while args.callback() == False:
 #        time.sleep(1)
