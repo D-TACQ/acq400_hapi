24b555d9c3b6c40b1b70fa34c2611a379e3ef24b
Peter Milne <peter.milne@d-tacq.com>
2023-03-09 14:34:43 +0000

    make a series of waveforms, each with an ID channel that shows a difference from the previous waveform. Useful for showing a sequence of different waveforms.  On branch master 	new file:   user_apps/utils/make_awg_data_id_set.py

---
+++ user_apps/utils/make_awg_data_id_set.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+
+'''
+Created on 3 Nov 2020
+
+@author: pgm
+
+make_awg_data_id_set fname
+
+Makes a series of waveforms, each with one of the channels as an "ID"
+
+regular: sin at +1V
+id     : cos at -1V
+
+'''
+
+import numpy as np
+import argparse
+
+def make_awg_data(args):
+    x = np.linspace(0, 8*np.pi, args.len)
+    y = args.amp * np.sin(x) 
+    volts = np.zeros([args.len, args.nchan])
+    for ch in range(args.nchan):
+        volts[:,ch] = np.add(y, args.offset + ch*args.offset_by_channel)
+    
+    basev = (volts * 32767/10).astype(np.int16)
+    
+    cos = args.amp * np.sin(x+np.pi/2)
+    cosv = (np.add(cos, -args.offset) * 32767/10).astype(np.int16)
+    return(cosv, basev)
+    
+def next(args, ch):
+    if ch+1 < args.nchan:
+        return ch+1
+    else:
+        return ch
+    
+def make_id_set(args): 
+    cosv, basev = make_awg_data(args)
+    
+    for ch in range(0, args.nchan):
+        chv = basev
+        chv[:,ch] = cosv
+        cn = next(args,ch)
+        chv[:,cn] = basev[:,cn] - args.offset_c1*32767*(cn%4)/10/4
+        fn = f"{args.fname[0]}-{args.nchan}-{args.len}-{ch+1}.dat"
+        print(fn)
+        chv.tofile(fn)
+    
+    
+    
+    
+def run_main():
+    parser = argparse.ArgumentParser(description='make_awg_data')
+    parser.add_argument('--nchan',  default=16,     type=int,   help="number of channels in set")
+    parser.add_argument('--len',    default=100000, type=int,   help="number of samples in set")
+    parser.add_argument('--amp',    default=0.5,                help="amplitude in volts")
+    parser.add_argument('--ncycles', default=8,     type=int,   help="number of waveform cycles in set")
+    parser.add_argument('--offset_by_channel', default=0.0, type=float,     help="offset in volts *ch")
+    parser.add_argument('--offset', default=1.0, type=float,     help="global offset in volts ")
+    parser.add_argument('--offset_c1', default=1.0, type=float,  help="ch+1 output with this offset")
+    parser.add_argument('fname', nargs=1, help="filename root")
+    args = parser.parse_args()
+    make_id_set(args)
+
+# execution starts here
+
+
+if __name__ == '__main__':
+    run_main()
