c1d139c0df73dc3072d3de55cd0bf241a48be141
Peter Milne <peter.milne@d-tacq.com>
2023-08-16 17:21:11 +0100

    handles comments and reports errored lines 	new file:   CONFIGS/cycloid_scan_templates.txt 	modified:   waves/build_templates.py

--- waves/build_templates.py
+++ waves/build_templates.py
@@ -12,13 +12,25 @@ import re
 
 import templates.all_templates
 
+root_prefix = os.getenv("AWG_TEMPLATE_ROOT", "/tmp/AWG")
+
+line = 0
+
 def process(cmd):
+    global line
+    line += 1
+    if len(cmd[0]) == 0:
+        return -1
+    elif cmd[0] == '#':
+        return 0
+
     match = re.search(r'([A-Za-z0-9_-]+)/([0-9]+)', cmd[0])
     if match:
-        root = match.group(1)
+        root = f'{root_prefix}/{match.group(1)}'
         chan = match.group(2)
         cmd = cmd[1:]
         cmd.extend(('--root', root, '--ch', chan))
+        print(cmd)
         
     wave_commands = templates.all_templates.get_wave_commands()
     for key in sorted(wave_commands.keys()):
@@ -26,12 +38,13 @@ def process(cmd):
             wave_commands[key](cmd[1:])
             return 0
         
-    print(f'cmd {cmd} not found')
+    print(f'[{line}] ERROR cmd {" ".join(cmd)} not found')
     return 0
 
 def main():
-     while True:
-         process(input(">").split(" "))
+     while process(input(">").split(" ")) >= 0:
+         pass
+         
 
 if __name__ == "__main__":
     sys.exit(main())
