6a3095a9e8f9d6e8c154f4cfcc210a0e93faa859
sambelltacq <Sam.Bell@d-tacq.co.uk>
2023-12-15 11:48:09 +0000

    updated user_apps docstring

---
+++ user_apps/__init__.py
@@ -0,0 +1,9 @@
+"""user-oriented applications
+
+**comprising of** :
+    * acq2106/ : apps specific to acq2106
+    * acq400/ : generic acq400 series apps Look Here First
+    * special/ : ELF/FMC module specific apps
+    * utils/ : sundry plot and file utils
+    * web/ : web scripts
+"""
