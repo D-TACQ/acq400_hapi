d0b225d27fe26c50fc2e0dc4f31fc1d4d3a5c7a9
sambelltacq <Sam.Bell@d-tacq.co.uk>
2024-01-05 13:36:17 +0000

    Added next arg to set next active segment 	modified:   user_apps/web/compose_remote.py

--- user_apps/web/compose_remote.py
+++ user_apps/web/compose_remote.py
@@ -10,17 +10,22 @@ Load new Template::
 
 Compose::
 
-    ./user_apps/web/compose_remote.py --output=oneshot_rearm --nreps=10 --segment=A '5*AA 5*BB' acq1001_434
+    ./user_apps/web/compose_remote.py --output=oneshot_rearm --segment=A "5*AA 5*BB" acq1001_434
 
 Load new and compose::
 
-    ./user_apps/web/compose_remote.py  --file=CONFIGS/cycloid_scan_templates.txt --output=oneshot_rearm '5*AA 5*BB' acq1001_434
+    ./user_apps/web/compose_remote.py --file=CONFIGS/cycloid_scan_templates.txt --output=oneshot_rearm --segment=A "5*AA 5*BB" acq1001_434
+
+Set next segment::
+
+    ./user_apps/web/compose_remote.py --next=B
 
 """
 
 import argparse
 import requests
 import json
+import socket
 
 def run_main(args):
     if not args.port:
@@ -51,7 +56,7 @@ def run_main(args):
         }
         send_to_endpoint(url, payload)
 
-    if args.output and args.pattern:
+    if args.output and args.pattern and args.segment:
         payload = {
             'action' : 'awg_compose',
             'data': {
@@ -63,6 +68,8 @@ def run_main(args):
         }
         send_to_endpoint(url, payload)
 
+    if args.next:
+        send_to_port(args.uut, 54210, args.next)
 
 def send_to_endpoint(url, payload):
     print(f"\nSending to {url}")
@@ -74,6 +81,13 @@ def send_to_endpoint(url, payload):
     except Exception as e:
         print(f"Error: Unable to post to {url}")
 
+def send_to_port(host, port, message):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.connect((host, port))
+        sock.sendall(bytes(message, "utf-8"))
+    print(f"Sent {message} to {host}:{port}")
+
+
 def get_parser():
     parser = argparse.ArgumentParser(description='Remote control to composer webapp')
     parser.add_argument('--port', default=None, help="Set api port")
@@ -82,6 +96,8 @@ def get_parser():
                          oneshot_rearm, oneshot, continuous or a filename")
     parser.add_argument('--nreps', default='', help="Number of pattern repetitions")
     parser.add_argument('--segment', default='', choices=['A', 'B', 'C', 'D', 'E'], help="Set segment")
+    parser.add_argument('--next', choices=['A', 'B', 'C', 'D', 'E'], help="Set next active segment")
+
     parser.add_argument('pattern', nargs='*', help="Pattern to compose ie 5*AA 5*BB")
     parser.add_argument('uut', help="uut hostname")
     return parser
