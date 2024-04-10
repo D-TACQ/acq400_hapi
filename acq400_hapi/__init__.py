270e6cc5f415fd2ca4b6263b0859721e177295fe
Peter Milne <peter.milne@d-tacq.com>
2024-04-10 08:07:37 +0100

    "comprised of" preffered to "comprising of" (comprising .. without the "of" would be OK too.
    	modified:   ../../acq400_hapi/__init__.py

--- acq400_hapi/__init__.py
+++ acq400_hapi/__init__.py
@@ -1,6 +1,6 @@
 """Connectivity package for D-Tacq uuts
 
-**comprising of** :
+**comprised of** :
     * acq400.py : Acq400 class, represents an ACQ400 UUT
     * acq400_ui.py : common user interface elements for apps
     * netclient.py : Netclient class, TCP socket wrapper
