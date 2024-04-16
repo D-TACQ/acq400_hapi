dfd0e97d3580ab3df68d7cbb0c85cb327cfd9ade
Peter Milne <peter.milne@d-tacq.com>
2024-04-16 08:57:43 +0100

    get_sys_info() to show uut payload

--- user_apps/acq400/acq400_hello_world.py
+++ user_apps/acq400/acq400_hello_world.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 '''
 Created on 19 Jun 2021
 
@@ -9,7 +10,9 @@ import argparse
 
 def get_parser():
     parser = argparse.ArgumentParser(description='remote test script') 
-    parser.add_argument('-v','--verbose', default=0, help="show more info")
+    parser.add_argument('-v','--verbose',  default=0, type=int, help="show more info")
+    parser.add_argument('-s','--sys_info', default=1, type=int, help="show sys_info")
+    parser.add_argument('-t','--site_svc', default=0, type=int, help="show site services")
     parser.add_argument('uuts', nargs='+', help="uut[s]")
     return parser
    
@@ -26,8 +29,11 @@ def run_main(args):
     uuts = [acq400_hapi.factory(u) for u in args.uuts]
     
     for u in uuts:
-        sites = "".join([site_model(u, k) for k in sorted(u.svc)]).rstrip(',')
-        print("SUCCESS: {} {}".format(u.uut, sites))
+        if args.sys_info == 1:
+            print(u.get_sys_info())
+        if args.site_svc == 1:
+            sites = "".join([site_model(u, k) for k in sorted(u.svc)]).rstrip(',')
+            print("SUCCESS: {} {}".format(u.uut, sites))
     
 
 
