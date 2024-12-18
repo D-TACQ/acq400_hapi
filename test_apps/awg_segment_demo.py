24bf9adfeed0fdf63ea2d1639ae498977f9d9945
Peter Milne <peter.milne@d-tacq.com>
2024-12-18 08:47:02 +0000

    remove unwanted anatrg, agilent fg  On branch master 	modified:   test_apps/awg_segment_demo.py

--- test_apps/awg_segment_demo.py
+++ test_apps/awg_segment_demo.py
@@ -10,48 +10,11 @@ Usage:
 """
 import argparse
 import time
-from acq400_hapi import factory, pv, Agilent33210A
+import acq400_hapi
+from acq400_hapi import pv
 from epics import caget, caput, caput_many
 
 from user_apps.utils.wavegen import WaveGen
-
-#classes
-class AnatrgHelper():
-
-    def __init__(self, uutname, site):
-        self.pvbase = f"{uutname}:{site}:ANATRG:"
-
-    def set(self, channels, level1, level2=None, group=None, mode="rising"):
-        print(f"Setting Chans {channels} {level1}V {mode} group {group}")
-        map = {}
-        chan_mode = "{pvbase}{chan:02}:M"
-        chan_level1 = "{pvbase}{chan:02}:L1"
-        chan_level2 = "{pvbase}{chan:02}:L2"
-        chan_group = "{pvbase}GROUP:{chan:02}"
-        
-        for chan in channels:
-            map[chan_mode.format(pvbase=self.pvbase, chan=chan)] = mode
-            map[chan_level1.format(pvbase=self.pvbase, chan=chan)] = level1
-            if level2 != None: map[chan_level2.format(pvbase=self.pvbase, chan=chan)] = level2
-            if group != None: map[chan_group.format(pvbase=self.pvbase, chan=chan)] = group
-
-        caput_many(map.keys(), map.values(), wait=True)
-
-    def set_group_mode(self, group=0):
-        caput(f"{self.pvbase}GROUP_MODE", group)
-
-    def set_all(self, level1=0, level2=0, mode='none'):
-        print(f"Setting all {level1}V {level2}V {mode}")
-        caput(f"{self.pvbase}ALL:L1", level1)
-        caput(f"{self.pvbase}ALL:M", mode)
-        caput(f"{self.pvbase}ALL:L2", level2)
-        caput(f"{self.pvbase}ALL:SET", 1)
-
-    def group_all(self, group=True):
-        if group:
-            caput(f"{self.pvbase}GROUP:ALL:SET", 1)
-        else:
-            caput(f"{self.pvbase}GROUP:ALL:CLR", 1)
 
 class DotDict(dict):
     __delattr__ = dict.__delitem__
@@ -89,10 +52,8 @@ def get_segments(char):
 def run_main(args):
     print(args)
 
-    ao_uut = factory(args.ao)
-    ai_uut = factory(args.ai)
-
-    EvtSrc = DotDict({'TRG': 0, 'MOD': 3})
+    ao_uut = acq400_hapi.factory(args.ao)
+    ai_uut = acq400_hapi.factory(args.ai)
 
     print(f"Testing {ai_uut.uut} Site {args.awg_site} using {ao_uut.uut} Site {args.ao_site}")
    
@@ -109,18 +70,8 @@ def run_main(args):
     nchan = int(ai_uut[args.awg_site].NCHAN)
     segments = get_segments(args.max_seg)
     groups = get_groups(nchan, 8)
-    ai_uut.s0.evt_src_d0 = EvtSrc[args.ebs0]
     
     wait_for_run(ai_uut)
-
-    if not args.leave_atd:
-        ana = AnatrgHelper(ai_uut.uut, args.awg_site)
-        ana.set_all()
-
-        for idx, chans in enumerate(groups):
-            level = 8.5 - idx
-            ana.set(chans, level1=level, mode='rising')
-            time.sleep(1)
 
     pretrg = pv(ao_uut.s0.SIG_SRC_TRG_0)
     pretrg = pretrg if pretrg != 'NONE' else 'EXT'
@@ -146,14 +97,15 @@ def run_main(args):
     ao_uut.s0.SIG_SRC_TRG_0 = pretrg
 
     while True:
-        for segment in segments:
-            print(f"Setting segment {segment} as active")
-            ao_uut.set_segment(segment)
+        with acq400_hapi.Netclient(ao_uut.uut, acq400_hapi.AcqPorts.AWG_SEGMENT_SELECT) as nc:
+            for segment in segments:
+                print(f"Setting segment {segment} as active")
+                nc.send(segment)
 
-            if args.pause:
-                input('Press Enter to continue . . .')
-            else:
-                time.sleep(1)
+                if args.pause:
+                    input('Press Enter to continue . . .')
+                else:
+                    time.sleep(1)
 
 def get_parser():    
     parser = argparse.ArgumentParser(description="anatrg demo script")
@@ -173,7 +125,6 @@ def get_parser():
     parser.add_argument('--spos', default="+0:4000,500", help="waveform start position")
     parser.add_argument('--scale', default="0.2", help="waveform voltage scale")
     parser.add_argument('--noload', default=0, help="don't load the WF (maybe already done)")
-    parser.add_argument('--leave_atd', default=0, help="dont' change atd settings (maybe already done)")
     return parser
 
 if __name__ == '__main__':
