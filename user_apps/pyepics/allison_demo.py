ad935bcd4a16d74307f43ea0f21728895be19d89
sambelltacq <Sam.Bell@d-tacq.co.uk>
2024-12-23 18:04:51 +0000

    Added --stack, --slow and --tolerance args 	modified:   user_apps/pyepics/allison_demo.py

--- user_apps/pyepics/allison_demo.py
+++ user_apps/pyepics/allison_demo.py
@@ -5,7 +5,7 @@ ALLISON DEMO
 
 
 Usage:
-    ./user_apps/pyepics/allison_demo.py acq2106_176 --translen=20480 --mask=1-6,17-20
+    ./user_apps/pyepics/allison_demo.py acq2106_176 --translen=20480 --mask=1-6,17-20 --slow=1,2,3,4 --stack=5
 
 """
 
@@ -249,6 +249,55 @@ def find_event_signatures(dataset, ssb):
                 return indices
     return []
 
+def all_plot(dataset, pchan, schan, jump_indices=[]):
+    fig1, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 5))
+    fig1.canvas.manager.set_window_title('All plot')
+    for chan, data in dataset.chan.items():
+        if pchan != None and chan not in pchan: continue
+        label = f"Chan {chan}"
+        print(f"Plotting {label}")
+        ax1.plot(data, label=label,  markevery=jump_indices, marker='v')
+    ax1.set_title('Data Channels')
+    ax1.legend(loc="upper left")
+
+    for spad, data in dataset.spad.items():
+        if schan != None and spad not in schan: continue
+        label = f"Spad {spad}"
+        print(f"Plotting {label}")
+        ax2.plot(data, label=label)
+    ax2.set_title('Spad Channels')
+    ax2.legend(loc="upper left")
+
+def slow_plt(chans, data, burstlen):
+    """Plot first value of each burst"""
+    plt.figure('Slow plot')
+    for chan in chans:
+        if chan not in data: continue
+        print(f"Slow plotting Chan {chan}")
+        plt.plot(data[chan][0::burstlen], label=f"chan {chan}")
+    plt.legend(loc="upper left")
+
+def stack_plt(chans, data, burstlen, offset=0):
+    """Plot each burst stacked"""
+    for chan in chans:
+        if chan not in data: continue
+        cursor = 0
+        datalen = len(data[chan])
+        plt.figure(f"Stack plot Chan {chan}")
+        print(f"Stack plotting Chan {chan}")
+        while cursor + burstlen <= datalen:
+            plt.plot(data[chan][cursor: cursor + burstlen] + (cursor // burstlen) * offset) #fix me 
+            cursor += burstlen
+
+def find_jump_indices(data, burstlen, tolerance):
+    for chan in data:
+        diffs = np.diff(data[chan][0::burstlen])
+        mean = np.mean(diffs)
+        tolerance = np.abs(mean * tolerance)
+        indices = np.where(np.abs(diffs - mean) > tolerance)[0]
+        if len(indices) < 1: return []
+        nonconsectives = np.insert(np.diff(indices) != 1, 0, True)
+        return indices[nonconsectives] * burstlen
 
 # Stars here
 
@@ -290,23 +339,25 @@ def run_main(args):
         unique_es_diffs = np.unique(np.diff(dataset.es_indices))
         print(f"Event signature intervals {unique_es_diffs}")
 
+    burstlen = int(pvs.RTM_TRANSLEN.get()) - 1
+
+    #find jump indices
+    jump_indices = []
+    if args.tolerance != None:
+        jump_indices = find_jump_indices(dataset.chan, burstlen, args.tolerance)
+        if len(jump_indices) > 1:
+            print(f"Warning: found jumps at indices {jump_indices}")
+
     #plotting here
-    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 5))
+    if args.all:
+        all_plot(dataset, args.pchan, args.pspad, jump_indices)
 
-    for chan, data in dataset.chan.items():
-        if args.pchan != None and chan not in args.pchan: continue
-        label = f"Chan {chan}"
-        print(f"Plotting {label}")
-        ax1.plot(data, label=label)
-    ax1.legend(loc="upper left")
+    if args.slow != None:
+        slow_plt(args.slow, dataset.chan, burstlen)
 
-    for spad, data in dataset.spad.items():
-        if args.pspad != None and spad not in args.pspad: continue
-        label = f"Spad {spad}"
-        print(f"Plotting {label}")
-        ax2.plot(data, label=label)
-    ax2.legend(loc="upper left")
-
+    if args.stack != None:
+        stack_plt(args.stack, dataset.chan, burstlen)
+    
     plt.tight_layout()
     plt.show()
 
@@ -315,6 +366,7 @@ def run_main(args):
 
 def list_of_channels(arg):
     if arg.lower() == 'all': return arg
+    if arg.lower() == 'none': return None
     channels = []
     for chan in arg.split(','):
         if '-' in chan:
@@ -330,17 +382,17 @@ def valid_translen(arg):
     raise argparse.ArgumentTypeError(f"Invalid value: {arg}. Must be divisible by 1024.")
 
 def get_parser():
-    parser = argparse.ArgumentParser(description="Plotter for FNAL")
+    parser = argparse.ArgumentParser(description="Alison Plotter")
 
-    parser.add_argument('--pchan', default='all', type=list_of_channels, help="Channels to plot")
-    parser.add_argument('--pspad', default=[1], type=list_of_channels, help="Spads to plot (0 indexed)")
+    parser.add_argument('--pchan', default='1-5', type=list_of_channels, help="Channels to plot in all plot")
+    parser.add_argument('--pspad', default=[1], type=list_of_channels, help="Spads to plot (0 indexed) in all plot")
 
     parser.add_argument('--stream', default=1, type=int, help="to stream or not to stream")
     parser.add_argument('--maxtime', default=None, type=int, help="Stream max time")
     parser.add_argument('--maxbytes', default=None, type=int, help="Stream max bytes")
 
-    parser.add_argument('--stack', default='5', type=list_of_channels, help="channels to stack plot")
-    parser.add_argument('--slow', default='1-4', type=list_of_channels, help="channels to slow plot")
+    parser.add_argument('--stack', default='none', type=list_of_channels, help="channels to stack plot")
+    parser.add_argument('--slow', default='none', type=list_of_channels, help="channels to slow plot")
     parser.add_argument('--all', default=1, type=int, help="plot all")
 
     parser.add_argument('--scan_steps', default=400, type=int, help="Ramp scan_steps")
@@ -348,6 +400,7 @@ def get_parser():
     parser.add_argument('--ao_site', default=5, type=int, help="Site with the ao")
     parser.add_argument('--translen', default=None, type=valid_translen, help="Burst length: any number 1024 - 22000")
     parser.add_argument('--mask', default="1-6,17-20", type=list_of_channels, help="channels in the mask")
+    parser.add_argument('--tolerance', default=None, type=float, help="Jump index tolerance value (ie 0.1)")
 
     parser.add_argument('uut', help="uut name")
     return parser
