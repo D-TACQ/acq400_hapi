4c2c81e3103cb49e2b791adbb32f009da2313bcc
sambelltacq <Sam.Bell@d-tacq.co.uk>
2024-12-23 16:52:03 +0000

    Replaced hapi style interface with more flat epics style 	modified:   user_apps/pyepics/allison_demo.py

--- user_apps/pyepics/allison_demo.py
+++ user_apps/pyepics/allison_demo.py
@@ -10,14 +10,56 @@ Usage:
 """
 
 import argparse
+import socket
+import time
+
 import numpy as np
 from matplotlib import pyplot as plt
+import epics
 
-from pprint import pprint
-
-from acq400_hapi.acq400e import acq400e, DotDict, States
 
 # Classes
+
+class DotDict(dict):
+    __delattr__ = dict.__delitem__
+    __getattr__ = dict.__getitem__
+    __setattr__ = dict.__setitem__
+
+class PVHelper:
+    def __init__(self, uut, ao_site):
+        self.uut = uut
+        self.datafile = f"{uut}.dat"
+        self.stop_flag = False
+
+        self.cstate = None
+        cstate_callback = lambda value, **kwargs: setattr(self, 'cstate', value)
+        self.cstate_pv = epics.PV("{uut}:MODE:CONTINUOUS:STATE".format(uut=uut), callback=cstate_callback, auto_monitor=True)
+
+        self.tstate = None
+        tstate_callback = lambda value, **kwargs: setattr(self, 'tstate', value)
+        self.tstate_pv = epics.PV("{uut}:MODE:TRANS_ACT:STATE".format(uut=uut), callback=tstate_callback, auto_monitor=True)
+
+        pvs = [
+            (0, "STREAM_SUBSET_MASK"),
+            (0, "NCHAN"),
+            (0, "SPAD:LEN:r"),
+            (0, "SSB"),
+            (0, "AO:STEP:CURSOR"),
+            (1, "data32"),
+            (1, "RTM_TRANSLEN"),
+            (ao_site, 'AO:STEP:1'),
+            (ao_site, 'AO:STEP:2'),
+            (ao_site, 'AO:STEP:3'),
+            (ao_site, 'AO:STEP:4'),
+            (ao_site, 'AO:STEP:1:EN'),
+            (ao_site, 'AO:STEP:2:EN'),
+            (ao_site, 'AO:STEP:3:EN'),
+            (ao_site, 'AO:STEP:4:EN'),
+        ]
+
+        for (site, pvname) in pvs:
+            setattr(self, pvname.replace(':', '_'), epics.PV(f"{uut}:{site}:{pvname}"))
+
 class MaskHelper(DotDict):
     """helper for channel mask"""
     def __init__(self, mask):
@@ -44,16 +86,6 @@ class MaskHelper(DotDict):
 
 
 # Functions
-
-def monitor_cursor(uut, scan_steps):
-    cursor_pv = "{uut}:0:AO:STEP:CURSOR"
-
-    def cursor_callback(value, **kwargs):
-        if value >= scan_steps and uut.cstate != States.IDLE.value:
-            print('Requesting stop')
-            uut.stop_flag = True
-
-    uut.monitor(cursor_pv, cursor_callback)
 
 def get_data_format(nchan, datalen, schan=0, mask=[]):
     """generates dtype for expected data"""
@@ -101,24 +133,76 @@ def get_data_format(nchan, datalen, schan=0, mask=[]):
 
     return format
 
-def make_ramp(uut, site, amplitude, scan_steps):
+def setup_ramp(pvs, amplitude, scan_steps):
     ramp_up = np.linspace(-amplitude, amplitude, scan_steps)
     ramp_dn = np.linspace(amplitude, -amplitude, scan_steps)
     cup = amplitude * np.cos(np.linspace(0, np.pi, scan_steps))
     sup = amplitude * np.sin(np.linspace(0, np.pi, scan_steps))
 
-    uut[site].AO_STEP_1 = ramp_up
-    uut[site].AO_STEP_2 = ramp_dn
-    uut[site].AO_STEP_3 = cup
-    uut[site].AO_STEP_4 = sup
+    pvs.AO_STEP_1.put(ramp_up, wait=True)
+    pvs.AO_STEP_2.put(ramp_dn, wait=True)
+    pvs.AO_STEP_3.put(cup, wait=True)
+    pvs.AO_STEP_4.put(sup, wait=True)
 
-    uut[site].AO_STEP_1_EN = 1
-    uut[site].AO_STEP_2_EN = 1
-    uut[site].AO_STEP_3_EN = 1
-    uut[site].AO_STEP_4_EN = 1
+    pvs.AO_STEP_1_EN.put(1, wait=True)
+    pvs.AO_STEP_2_EN.put(1, wait=True)
+    pvs.AO_STEP_3_EN.put(1, wait=True)
+    pvs.AO_STEP_4_EN.put(1, wait=True)
 
-def start_ramp(uut):
-    uut.s0.AO_STEP_CURSOR = 0
+    pvs.AO_STEP_CURSOR.put(0, wait=True)
+
+def stream_to_disk(pvs, ssb, maxbytes=None, maxtime=None, update=True):
+        
+        LINE_UP = '\033[1A'
+        ERASE_LINE = '\033[2K'
+        bufferlen = ssb * 1024
+        buffer = bytearray(bufferlen)
+        byteview = memoryview(buffer).cast('B')
+        print("Starting Stream")
+
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            sock.connect((pvs.uut, 4210))
+            with open(pvs.datafile, 'wb') as fp:
+                index = 0
+                tbytes = 0
+                t0 = 0
+                while True:
+                    nbytes = sock.recv_into(byteview[index:])
+
+                    if nbytes == 0: 
+                        print('Error: uut has stoppped')
+                        break
+
+                    index += nbytes
+                    tbytes += nbytes
+
+                    if t0 == 0: t0 = time.time()
+
+                    if index >= bufferlen:
+                        if update:
+                            tt = time.time() - t0
+                            print(f"Streaming {int(tt)}s {(tbytes >> 20) / tt:.5f} MB/s > {pvs.datafile}")
+
+                        fp.write(buffer[:index])
+                        index = 0
+
+                        if maxtime and time.time() - t0 > maxtime:
+                            print('Stream reached max time')
+                            break
+
+                        if maxbytes and tbytes >= maxbytes:
+                            print('Stream reached max bytes')
+                            break
+
+                        if pvs.stop_flag:
+                            print("Stream received stop signal")
+                            break
+
+                        if update: print(LINE_UP + ERASE_LINE , end="")
+
+                fp.flush()
+            sock.shutdown(socket.SHUT_RDWR)
+        print(f"{tbytes:,} bytes {tbytes // ssb:,} samples total")
 
 def read_from_disk(filename, data_format):
 
@@ -153,13 +237,9 @@ def find_event_signatures(dataset, ssb):
     ]
     if ssb % 4 != 0:
         print('Warning unable to find es as sample size is not divisble by 4')
+        #TODO: trim to fit (uint8?)
         return []
-
-    #TODO find how to trim channel when using structured ndarry
-    # data[:-trim] ? if not divisble by unint32
-    #trim = ssb % 4
-    #data8 = dataset.data.view(np.uint8)
-
+    
     chans32 = ssb // 4
     data32 = dataset.data.view(np.uint32).reshape(-1, chans32).T
     for dat in data32:
@@ -169,37 +249,48 @@ def find_event_signatures(dataset, ssb):
                 return indices
     return []
 
+
 # Stars here
 
 def run_main(args):
-    print(args)
-    uut = acq400e(args.uut)
+    pvs = PVHelper(args.uut, args.ao_site)
     mask = MaskHelper(args.mask)
 
     args.maxtime = None if args.maxbytes else args.maxtime
     args.pchan = None if args.pchan =='all' else args.pchan
     args.pspad = None if args.pspad =='all' else args.pspad
     
-    uut.s0.STREAM__SUBSET__MASK = mask.hex
-    if args.translen: uut.s1.RTM__TRANSLEN = args.translen
+    pvs.STREAM_SUBSET_MASK.put(mask.hex, wait=True)
+    if args.translen: pvs.RTM_TRANSLEN.put(args.translen, wait=True)
 
-    monitor_cursor(uut, args.scan_steps)
+    #monitor cursor and stop when complete
+    def cursor_callback(value, **kwargs):
+        if value >= args.scan_steps and pvs.cstate != 0:
+            print('Requesting stop')
+            pvs.stop_flag = True
 
-    data_format = get_data_format(int(uut.s0.NCHAN), int(uut.s1.data_len), int(uut.s0.SPAD_LEN_r), mask.list)
+    epics.PV("{uut}:0:AO:STEP:CURSOR".format(uut=args.uut), callback=cursor_callback, auto_monitor=True)
 
-    make_ramp(uut, args.ao_site, args.amplitude, args.scan_steps)
-    start_ramp(uut)
+    #Generate data format dtype
+    nchan = int(pvs.NCHAN.get())
+    chanlen = 4 if int(pvs.data32.get()) else 2
+    spadlen = int(pvs.SPAD_LEN_r.get())
+    data_format = get_data_format(nchan, chanlen, spadlen, mask.list)
 
+    #Setup ramp 
+    setup_ramp(pvs, args.amplitude, args.scan_steps)
+
+    #stream data to disk then read in data
     if args.stream:
-        uut.stream_to_disk(data_format.samp_w, maxtime=args.maxtime, maxbytes=args.maxbytes)
+        stream_to_disk(pvs, data_format.samp_w, maxtime=args.maxtime, maxbytes=args.maxbytes)
+    dataset = read_from_disk(pvs.datafile, data_format)
 
-    dataset = read_from_disk(uut.datafile, data_format)
-
+    #find unique es intervals
     if len(dataset.es_indices) > 0:
         unique_es_diffs = np.unique(np.diff(dataset.es_indices))
         print(f"Event signature intervals {unique_es_diffs}")
 
-
+    #plotting here
     fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 5))
 
     for chan, data in dataset.chan.items():
@@ -218,6 +309,7 @@ def run_main(args):
 
     plt.tight_layout()
     plt.show()
+
 
 # Argparser
 
